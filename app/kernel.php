d614e92bada7ef5d98087e05ee60c8a087981b71
dadajuice <davidt2003@msn.com>
2016-12-22 17:25:02 -0500

    Added main system file with global mapping

---
+++ app/kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+define('ROOT_DIR', __DIR__ . '/..');
+require ROOT_DIR . '/vendor/autoload.php';
+
+use Zephyrus\Application\Configuration;
+use Zephyrus\Security\Session;
+use Zephyrus\Network\Request;
+use Zephyrus\Security\IntrusionDetection;
+use Zephyrus\Security\Log;
+
+define('PAGE_MAX_ENTITIES', 50);
+include('functions.php');
+
+//include('error_handlers_dev.php');
+
+//$session = Session::getInstance(Configuration::getSessionConfiguration());
+//$session->start();
+
+/*if (Configuration::getIdsConfiguration('active')) {
+    $ids = IntrusionDetection::getInstance(Configuration::getIdsConfiguration());
+    $ids->onDetection(function ($data) {
+        Log::addSecurity("IDS detection : " . json_encode($data));
+    });
+}*/
