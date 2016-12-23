4c1e25b6e41a8b14efe7fc54c5498ba8fda0093d
dadajuice <davidt2003@msn.com>
2016-12-23 16:34:33 -0500

    Enabled encrypted session feature

--- app/kernel.php
+++ app/kernel.php
@@ -5,17 +5,12 @@ require ROOT_DIR . '/vendor/autoload.php';
 
 use Zephyrus\Application\Configuration;
 use Zephyrus\Security\Session;
-use Zephyrus\Network\Request;
-use Zephyrus\Security\IntrusionDetection;
-use Zephyrus\Security\SystemLog;
-
-define('PAGE_MAX_ENTITIES', 50);
-include('zephyrus/functions.php');
 
 include('handlers.php');
+include('zephyrus/functions.php');
 
-//$session = Session::getInstance(Configuration::getSessionConfiguration());
-//$session->start();
+$session = Session::getInstance(Configuration::getSessionConfiguration());
+$session->start();
 
 /*if (Configuration::getIdsConfiguration('active')) {
     $ids = IntrusionDetection::getInstance(Configuration::getIdsConfiguration());
