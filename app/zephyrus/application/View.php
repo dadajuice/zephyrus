16acb0cd5985841eb7c826d837ea9687cec2f4f3
dadajuice <davidt2003@msn.com>
2016-12-26 13:41:57 -0500

    Added format function to Pug

--- app/zephyrus/application/View.php
+++ app/zephyrus/application/View.php
@@ -71,7 +71,90 @@ class View
         $args['_val'] = function($fieldId, $defaultValue = "") {
             return _val($fieldId, $defaultValue);
         };
+        $args['format'] = $this->addFormatFunction();
         $this->pug->share($args);
+    }
+
+    private function addFormatFunction()
+    {
+        return function($type, $value, ...$args) {
+            $argc = count($args);
+            switch ($type) {
+                case 'filesize' :
+                    return Formatter::formatHumanFileSize($value);
+                case 'time' :
+                    return Formatter::formatTime(($value instanceof \DateTime) ? $value : new \DateTime($value));
+                case 'elapsed':
+                case 'french_elapsed' :
+                    return Formatter::formatElapsedDateTime(($value instanceof \DateTime) ? $value : new \DateTime($value));
+                case 'datetime':
+                case 'french_datetime' :
+                    if ($argc == 0) {
+                        return Formatter::formatFrenchDateTime(($value instanceof \DateTime) ? $value : new \DateTime($value));
+                    } elseif ($argc == 1) {
+                        return Formatter::formatFrenchDateTime(($value instanceof \DateTime) ? $value : new \DateTime($value), $args[0]);
+                    } elseif ($argc == 2) {
+                        return Formatter::formatFrenchDateTime(($value instanceof \DateTime) ? $value : new \DateTime($value), $args[0], $args[1]);
+                    } else {
+                        return 'French datetime format must between 0 and 2 arguments (' . $argc . ' provided)';
+                    }
+                case 'date':
+                case 'french_date' :
+                    if ($argc == 0) {
+                        return Formatter::formatFrenchDate(($value instanceof \DateTime) ? $value : new \DateTime($value));
+                    } elseif ($argc == 1) {
+                        return Formatter::formatFrenchDate(($value instanceof \DateTime) ? $value : new \DateTime($value), $args[0]);
+                    } elseif ($argc == 2) {
+                        return Formatter::formatFrenchDate(($value instanceof \DateTime) ? $value : new \DateTime($value), $args[0], $args[1]);
+                    } else {
+                        return 'French date format must between 0 and 2 arguments (' . $argc . ' provided)';
+                    }
+                case 'period':
+                case 'french_period' :
+                    if ($argc == 2) {
+                        return Formatter::formatFrenchPeriod(
+                            ($value instanceof \DateTime) ? $value : new \DateTime($value),
+                            ($args[0] instanceof \DateTime) ? $args[0] : new \DateTime($args[0]));
+                    } else {
+                        return 'French period format must have 2 arguments (' . $argc . ' provided)';
+                    }
+                case 'percent' :
+                    if ($argc == 0) {
+                        return Formatter::formatPercent($value);
+                    } elseif ($argc == 1) {
+                        return Formatter::formatPercent($value, $args[0]);
+                    } elseif ($argc == 2) {
+                        return Formatter::formatPercent($value, $args[0], $args[1]);
+                    } else {
+                        return 'Percent format must between 0 and 2 arguments (' . $argc . ' provided)';
+                    }
+                case 'money' :
+                    if ($argc == 0) {
+                        return Formatter::formatMoney($value);
+                    } elseif ($argc == 1) {
+                        return Formatter::formatMoney($value, $args[0]);
+                    } elseif ($argc == 2) {
+                        return Formatter::formatMoney($value, $args[0], $args[1]);
+                    }  elseif ($argc == 3) {
+                        return Formatter::formatMoney($value, $args[0], $args[1], $args[2]);
+                    } else {
+                        return 'Money format must have between 0 and 3 arguments (' . $argc . ' provided)';
+                    }
+                case 'decimal' :
+                    if ($argc == 0) {
+                        return Formatter::formatDecimal($value);
+                    } elseif ($argc == 1) {
+                        return Formatter::formatDecimal($value, $args[0]);
+                    } elseif ($argc == 2) {
+                        return Formatter::formatDecimal($value, $args[0], $args[1]);
+                    }  elseif ($argc == 3) {
+                        return Formatter::formatDecimal($value, $args[0], $args[1], $args[2]);
+                    } else {
+                        return 'Decimal format must have between 0 and 3 arguments (' . $argc . ' provided)';
+                    }
+            }
+            return 'FORMAT TYPE [' . $type . '] NOT DEFINED !';
+        };
     }
 
     private function addPagerKeyword()
