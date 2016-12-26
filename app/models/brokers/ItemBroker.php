3429d5b4df5a9a3cab645b7b3c13921786da55f7
dadajuice <davidt2003@msn.com>
2016-12-25 22:14:37 -0500

    Added countAll method for pager

--- app/models/brokers/ItemBroker.php
+++ app/models/brokers/ItemBroker.php
@@ -5,8 +5,18 @@ use Zephyrus\Database\Broker;
 
 class ItemBroker extends Broker
 {
+    private static $SQL_COUNT_ALL = "SELECT COUNT(*) FROM item";
     private static $SQL_SELECT_ALL = "SELECT * FROM item";
     private static $SQL_INSERT = "INSERT INTO item (name, price) VALUES(?, ?)";
+
+    /**
+     * @return int
+     */
+    public function countAll()
+    {
+        $row = $this->selectUnique(self::$SQL_COUNT_ALL);
+        return (int) $row[0];
+    }
 
     /**
      * @return Item[]
