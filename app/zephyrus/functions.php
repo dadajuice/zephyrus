c1ad7c0f286d02a7cae863b1712de47092184a5d
David Tucker <davidt2003@msn.com>
2016-12-30 20:32:01 -0500

    Moved recursiveGlob to the framework helper functions

--- app/zephyrus/functions.php
+++ app/zephyrus/functions.php
@@ -140,3 +140,19 @@ function getCurrentUrl()
 {
     return Request::getBaseUrl() . Request::getUri();
 }
+
+/**
+ * Performs a normal glob pattern search, but enters directories recursively.
+ *
+ * @param string $pattern
+ * @param int $flags
+ * @return array
+ */
+function recursiveGlob($pattern, $flags = 0)
+{
+    $files = glob($pattern, $flags);
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, recursiveGlob($dir.'/'.basename($pattern), $flags));
+    }
+    return $files;
+}
