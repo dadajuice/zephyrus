a7f0a012988e06ac47e3bc04096c91f49ea7cd02
David Tucker <davidt2003@msn.com>
2016-12-30 20:31:31 -0500

    Added simple class to locate namespace classes

---
+++ app/zephyrus/application/ClassLocator.php
@@ -0,0 +1,58 @@
+<?php namespace Zephyrus\Application;
+
+class ClassLocator
+{
+    /**
+     * Returns all class names (including complete namespace) for a given
+     * composer registered PSR-4 namespace.
+     *
+     * @param $namespace
+     * @return array
+     */
+    public static function getClassesInNamespace($namespace)
+    {
+        $files = scandir(self::getNamespaceDirectory($namespace));
+        $classes = array_map(function($file) use ($namespace){
+            return $namespace . '\\' . str_replace('.php', '', $file);
+        }, $files);
+        return array_values(array_filter($classes, function($possibleClass){
+            return class_exists($possibleClass);
+        }));
+    }
+
+    /**
+     * Returns all defined namespaces under PSR-4 section of the composer.json
+     * file.
+     *
+     * @return array
+     */
+    public static function getDefinedNamespaces()
+    {
+        $composerJsonPath = ROOT_DIR . DIRECTORY_SEPARATOR . 'composer.json';
+        $composerConfig = json_decode(file_get_contents($composerJsonPath));
+        $psr4 = "psr-4";
+        return (array) $composerConfig->autoload->$psr4;
+    }
+
+    /**
+     * Returns the complete real path for a given namespace based on the given
+     * definitions in the composer.json file.
+     *
+     * @param $namespace
+     * @return bool|string
+     */
+    private static function getNamespaceDirectory($namespace)
+    {
+        $composerNamespaces = self::getDefinedNamespaces();
+        $namespaceFragments = explode('\\', $namespace);
+        $undefinedNamespaceFragments = [];
+        while($namespaceFragments) {
+            $possibleNamespace = implode('\\', $namespaceFragments) . '\\';
+            if(array_key_exists($possibleNamespace, $composerNamespaces)){
+                return realpath(ROOT_DIR . DIRECTORY_SEPARATOR . $composerNamespaces[$possibleNamespace] . implode('/', $undefinedNamespaceFragments));
+            }
+            $undefinedNamespaceFragments[] = array_pop($namespaceFragments);
+        }
+        return false;
+    }
+}
