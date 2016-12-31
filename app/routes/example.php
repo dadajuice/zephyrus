c8d5369369872814a3ab416b6366a21a53ab43cd
David Tucker <davidt2003@msn.com>
2016-12-30 20:36:29 -0500

    Updated example route

--- app/routes/example.php
+++ app/routes/example.php
@@ -1,3 +1,10 @@
 <?php
 
-\Controllers\ExampleController::initializeRoutes($router);
+/**
+ * Simple example showcasing the basic usage of direct route definition instead
+ * of using routable controllers. The routes directory can be removed if you
+ * are only using controllers.
+ */
+$router->get("/example/static", function() {
+    echo "it works !";
+});
