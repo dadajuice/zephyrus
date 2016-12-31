89aa609d6daf04583826f1cb54549bc9a9f7cbf3
David Tucker <davidt2003@msn.com>
2016-12-30 22:30:09 -0500

    Updated SecureRouter to use the CsrfGuard class

--- app/zephyrus/security/SecureRouter.php
+++ app/zephyrus/security/SecureRouter.php
@@ -7,19 +7,14 @@ use Zephyrus\Exceptions\InvalidCsrfException;
 class SecureRouter extends Router
 {
     /**
-     * @var SecureRouter
+     * @var array
      */
-    private static $instance = null;
+    private $config;
 
-    /**
-     * @return SecureRouter
-     */
-    public static function getInstance()
+    public function __construct()
     {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
-        }
-        return self::$instance;
+        parent::__construct();
+        $this->config = Configuration::getSecurityConfiguration();
     }
 
     /**
@@ -41,11 +36,31 @@ class SecureRouter extends Router
      */
     protected function beforeCallback($route)
     {
-        SecureHeader::getInstance()->send();
-        CsrfFilter::getInstance()->filter();
+        /*SecureHeader::getInstance()->send();
 
         if (Configuration::getIdsConfiguration('active')) {
             IntrusionDetection::getInstance()->run();
+        }*/
+
+        if ($this->config['csrf_guard_enabled']) {
+            CsrfGuard::getInstance()->guard();
+            if ($this->config['csrf_guard_automatic_html']) {
+                ob_start();
+            }
+        }
+    }
+
+    /**
+     * Method called immediately after calling the associated route callback
+     * method. The default behavior is to do nothing. This should be overridden
+     * to customize any operation to be made right after the route callback.
+     *
+     * @param array $route
+     */
+    protected function afterCallback($route)
+    {
+        if ($this->config['csrf_guard_enabled'] && $this->config['csrf_guard_automatic_html']) {
+            echo CsrfGuard::getInstance()->injectForms(ob_get_clean());
         }
     }
 }
