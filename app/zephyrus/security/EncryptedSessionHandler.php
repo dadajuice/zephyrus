bc5cfe5ea04df89e55d14bf6d6570603fe2ba3d0
David Tucker <davidt2003@msn.com>
2017-01-01 21:08:18 -0500

    Updated encrypted session handler to use cryptography class methods

--- app/zephyrus/security/EncryptedSessionHandler.php
+++ app/zephyrus/security/EncryptedSessionHandler.php
@@ -3,18 +3,7 @@
 class EncryptedSessionHandler extends \SessionHandler
 {
     /**
-     * @var string Encryption algorithm (mcrypt compatible)
-     */
-    private $encryptionAlgorithm = MCRYPT_RIJNDAEL_128;
-
-    /**
-     * @var int Encryption initiation vector size
-     */
-    private $cryptIvSize;
-
-    /**
-     * @var string Encryption symmetric key created using the specified mcrypt
-     * algorithm in CBC mode.
+     * @var string Encryption symmetric key created using the specified algorithm
      */
     private $cryptKey;
 
@@ -30,18 +19,6 @@ class EncryptedSessionHandler extends \SessionHandler
     private $cookieKeyName;
 
     /**
-     * Makes sure the mcrypt extension is correctly installed.
-     *
-     * @throws \Exception
-     */
-    public function __construct()
-    {
-        if (!extension_loaded('mcrypt')) {
-            throw new \Exception(__CLASS__ . " needs the mcrypt PHP extension");
-        }
-    }
-
-    /**
      * Called on session_start, this method create the
      *
      * @param string $savePath
@@ -53,8 +30,6 @@ class EncryptedSessionHandler extends \SessionHandler
     {
         parent::open($savePath, $sessionName);
         $this->cookieKeyName = "key_$sessionName";
-        $this->cryptIvSize = mcrypt_get_iv_size($this->encryptionAlgorithm, MCRYPT_MODE_CBC);
-
         if (empty($_COOKIE[$this->cookieKeyName]) || strpos($_COOKIE[$this->cookieKeyName], ':') === false) {
             $this->createEncryptionCookie();
         } else {
@@ -101,28 +76,13 @@ class EncryptedSessionHandler extends \SessionHandler
     }
 
     /**
-     * Applies an algorithm used to encrypt session data on disk. Specified
-     * algorithm must be compatible with the Mcrypt extension.
-     *
-     * @param string $encryptionAlgorithm
-     */
-    public function setEncryptionAlgorithm($encryptionAlgorithm)
-    {
-        if (!in_array($encryptionAlgorithm, mcrypt_list_algorithms())) {
-            throw new \InvalidArgumentException("The provided algorithm must be compatible with the PHP Mcrypt extension");
-        }
-        $this->encryptionAlgorithm = $encryptionAlgorithm;
-    }
-
-    /**
      * @throws \Exception
      */
     private function createEncryptionCookie()
     {
-        $keyLength = mcrypt_get_key_size($this->encryptionAlgorithm, MCRYPT_MODE_CBC);
+        $keyLength = Cryptography::getEncryptionIvLength();
         $this->cryptKey = Cryptography::randomBytes($keyLength);
         $this->cryptAuth = Cryptography::randomBytes(32);
-
         $cookieSettings = session_get_cookie_params();
         setcookie(
             $this->cookieKeyName,
@@ -138,47 +98,39 @@ class EncryptedSessionHandler extends \SessionHandler
     }
 
     /**
-     * Encrypt the specified data using the defined algorithm in CBC mode. Also
-     * create an Hmac authentication hash.
+     * Encrypt the specified data using the defined algorithm. Also create an
+     * Hmac authentication hash.
      *
-     * @param $data
+     * @param string $data
      * @return string
      */
     private function encrypt($data)
     {
-        $iv = mcrypt_create_iv($this->cryptIvSize, MCRYPT_DEV_URANDOM);
-        $cipher = mcrypt_encrypt($this->encryptionAlgorithm,
-            $this->cryptKey,
-            $data,
-            MCRYPT_MODE_CBC,
-            $iv
-        );
-        $hmac = hash_hmac('sha256', $iv . $this->encryptionAlgorithm . $cipher, $this->cryptAuth);
+        $cipher = Cryptography::encrypt($data, $this->cryptKey);
+        list($iv, $cipher) = explode(':', $cipher);
+        $iv = base64_decode($iv);
+        $cipher = base64_decode($cipher);
+        $hmac = hash_hmac('sha256', $iv . Cryptography::getEncryptionAlgorithm() . $cipher, $this->cryptAuth);
         return $hmac . ':' . base64_encode($iv) . ':' . base64_encode($cipher);
     }
 
     /**
-     * Decrypt the specified data using the defined algorithm in CBC mode. Also
-     * verify the Hmac authentication hash. Returns false if Hmac validation
-     * fails.
+     * Decrypt the specified data using the defined algorithm. Also verify the
+     * Hmac authentication hash. Returns false if Hmac validation fails.
      *
-     * @param $data
-     * @return bool|string
+     * @param string $data
+     * @return string|bool
      */
     private function decrypt($data)
     {
         list($hmac, $iv, $cipher) = explode(':', $data);
-        $iv = base64_decode($iv);
-        $cipher = base64_decode($cipher);
-        $newHmac = hash_hmac('sha256', $iv . $this->encryptionAlgorithm . $cipher, $this->cryptAuth);
+        $ivReal = base64_decode($iv);
+        $cipherReal = base64_decode($cipher);
+        $newHmac = hash_hmac('sha256', $ivReal . Cryptography::getEncryptionAlgorithm() . $cipherReal, $this->cryptAuth);
         if ($hmac !== $newHmac) {
             return false;
         }
-        $decrypt = mcrypt_decrypt($this->encryptionAlgorithm,
-            $this->cryptKey,
-            $cipher,
-            MCRYPT_MODE_CBC,
-            $iv);
-        return rtrim($decrypt, "\0");
+        $decrypt = Cryptography::decrypt($iv . ':' . $cipher, $this->cryptKey);
+        return $decrypt;
     }
 }
