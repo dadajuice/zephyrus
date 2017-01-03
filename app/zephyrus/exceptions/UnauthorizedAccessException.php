12a0dba82d16108dd4c3e29a4231473dfe0aeef8
David Tucker <davidt2003@msn.com>
2017-01-03 15:30:57 -0500

    Updated UnauthorizedAccessException to reflect new authorization philosophy

--- app/zephyrus/exceptions/UnauthorizedAccessException.php
+++ app/zephyrus/exceptions/UnauthorizedAccessException.php
@@ -2,24 +2,24 @@
 
 class UnauthorizedAccessException extends \Exception
 {
-    public $allowedRoles = [];
+    public $requirements = [];
     public $uri;
 
-    public function __construct($uri = "", $allowedRoles = [])
+    public function __construct($uri = "", $requirements = [])
     {
         $this->uri = $uri;
-        $this->allowedRoles = $allowedRoles;
-        $data = implode(', ', $allowedRoles);
-        if (!empty($uri) && !empty($allowedRoles)) {
-            parent::__construct("Unauthorized access! Route [$uri] only available to [$data]");
+        $this->requirements = $requirements;
+        $data = implode(', ', $requirements);
+        if (!empty($uri) && !empty($requirements)) {
+            parent::__construct("Unauthorized access! Requirement(s) [$data] failed for route [$uri]");
         } else {
             parent::__construct("Unauthorized access!");
         }
     }
 
-    public function getAllowedRoles()
+    public function getRequirements()
     {
-        return $this->allowedRoles;
+        return $this->requirements;
     }
 
     public function getUri()
