c0145e4ec41444f4197a3648d12c7fb3f227b31f
David Tucker <davidt2003@msn.com>
2017-01-03 15:33:12 -0500

    Updated security file to include example authorization

--- app/security.php
+++ app/security.php
@@ -32,3 +32,10 @@ $csp->setBaseUri([Request::getBaseUrl()]);
 
 $header = SecureHeader::getInstance();
 $header->setContentSecurityPolicy($csp);
+
+$auth = \Zephyrus\Security\Authorization::getInstance();
+$auth->setMode(\Zephyrus\Security\Authorization::MODE_BLACKLIST);
+$auth->addSessionRequirement('admin', 'AUTH_LEVEL', 'admin');
+$auth->protect('/insert', \Zephyrus\Security\Authorization::ALL, 'admin');
+
+$_SESSION['AUTH_LEVEL'] = 'admin';
