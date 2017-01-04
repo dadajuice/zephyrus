ec815879c8b486f2f883ed1460f905c8b3ffc894
David Tucker <davidt2003@msn.com>
2017-01-03 20:27:25 -0500

    Kept only the ini config which are influenced by the env property

--- app/zephyrus/application/Bootstrap.php
+++ app/zephyrus/application/Bootstrap.php
@@ -7,8 +7,6 @@ class Bootstrap
     public static function start()
     {
         date_default_timezone_set(Configuration::getApplicationConfiguration('timezone'));
-        ini_set('post_max_size', Configuration::getApplicationConfiguration('upload_max_size'));
-        ini_set('upload_max_filesize', Configuration::getApplicationConfiguration('upload_max_size'));
         self::initializeErrorReporting();
         setlocale(LC_ALL, Configuration::getApplicationConfiguration('locale') . '.' . Configuration::getApplicationConfiguration('charset'));
         Session::getInstance()->start();
@@ -35,16 +33,6 @@ class Bootstrap
         $dev = (Configuration::getApplicationConfiguration('env') == 'dev');
         ini_set('display_startup_errors', $dev);
         ini_set('display_errors', $dev);
-        /*ini_set('html_errors', 1);
-        ini_set('log_errors', 1);
-        ini_set('ignore_repeated_errors', 1);
-        ini_set('ignore_repeated_source', 1);
-        ini_set('report_memleaks', 1);
-        ini_set('track_errors', 1);
-        ini_set('docref_root', 0);
-        ini_set('docref_ext', 0);*/
         ini_set('error_log', ROOT_DIR . '/logs/errors.log');
-        //ini_set('error_reporting', -1);
-        //ini_set('log_errors_max_len', 0);
     }
 }
