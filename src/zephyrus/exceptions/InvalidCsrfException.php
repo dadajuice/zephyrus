cd23eef9c15ec9375025eb20ad215af7ab334022
David Tucker <davidt2003@msn.com>
2017-01-06 21:47:21 -0500

    Modified namespace name

--- src/zephyrus/exceptions/InvalidCsrfException.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Zephyrus\Exceptions;
-
-class InvalidCsrfException extends \Exception
-{
-    public function __construct()
-    {
-        parent::__construct("Invalid CSRF token supplied");
-    }
-}
