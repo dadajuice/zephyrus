ae2092eb5ec7a7abf589964a49d31bf42a8d891e
David Tucker <davidt2003@msn.com>
2017-01-06 23:08:14 -0500

    Added tests for flash class

---
+++ tests/application/FlashTest.php
@@ -0,0 +1,42 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Application\Flash;
+
+class FlashTest extends TestCase
+{
+    public function testError()
+    {
+        Flash::error("alert");
+        $args = Flash::readAll();
+        self::assertEquals("alert", $args['flash']['error']);
+    }
+
+    public function testWarning()
+    {
+        Flash::warning("warning");
+        $args = Flash::readAll();
+        self::assertEquals("warning", $args['flash']['warning']);
+    }
+
+    public function testSuccess()
+    {
+        Flash::success("success");
+        $args = Flash::readAll();
+        self::assertEquals("success", $args['flash']['success']);
+    }
+
+    public function testNotice()
+    {
+        Flash::notice("notice");
+        $args = Flash::readAll();
+        self::assertEquals("notice", $args['flash']['notice']);
+    }
+
+    public function testInfo()
+    {
+        Flash::info("info");
+        $args = Flash::readAll();
+        self::assertEquals("info", $args['flash']['info']);
+    }
+}
