6ad1d4dbf9ad6a635e1cbab92c39159b0b3bb109
David Tucker <davidt2003@msn.com>
2017-01-06 20:23:44 -0500

    Added formatter class tests

---
+++ tests/application/FormatterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Application\Formatter;
+
+class FormatterTest extends TestCase
+{
+    public function testDefaultFormatDecimal()
+    {
+        $result = Formatter::formatDecimal(12.342743);
+        self::assertEquals("12,3427", $result);
+        $result = Formatter::formatDecimal(12.346);
+        self::assertEquals("12,346", $result);
+        $result = Formatter::formatDecimal(12.1);
+        self::assertEquals("12,10", $result);
+        $result = Formatter::formatDecimal(12);
+        self::assertEquals("12,00", $result);
+        $result = Formatter::formatDecimal(12.342743, 2, 2);
+        self::assertEquals("12,34", $result);
+        $result = Formatter::formatDecimal(12.346, 2, 2);
+        self::assertEquals("12,35", $result);
+        $result = Formatter::formatDecimal(12.13454, 0, 1);
+        self::assertEquals("12,1", $result);
+        $result = Formatter::formatDecimal(12.16454, 0, 1);
+        self::assertEquals("12,2", $result);
+        $result = Formatter::formatDecimal(12, 0, 0);
+        self::assertEquals("12", $result);
+    }
+}
