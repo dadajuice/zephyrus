076aae8e4d08f28c527c82b4e7996c8503f57289
David Tucker <davidt2003@msn.com>
2017-01-11 10:26:59 -0500

    Added tests for view builder class

--- tests/application/ViewBuilderTest.php
+++ tests/application/ViewBuilderTest.php
@@ -11,4 +11,18 @@ class ViewBuilderTest extends TestCase
         $output = $view->render(['item' => ['name' => 'Bob Lewis', 'price' => 12.30]]);
         self::assertEquals('<p>Example Bob Lewis</p>', $output);
     }
+
+    public function testViewRenderWithMoneyFormat()
+    {
+        $view = ViewBuilder::getInstance()->buildFromString('p Example #{item.name} is #{format(\'money\', item.price)}');
+        $output = $view->render(['item' => ['name' => 'Bob Lewis', 'price' => 12.30]]);
+        self::assertEquals('<p>Example Bob Lewis is 12,30 $</p>', $output);
+    }
+
+    public function testViewRenderWithMoneyFormat2()
+    {
+        $view = ViewBuilder::getInstance()->buildFromString('p Example #{item.name} is #{format(\'money\', item.price, 3)}');
+        $output = $view->render(['item' => ['name' => 'Bob Lewis', 'price' => 12.30]]);
+        self::assertEquals('<p>Example Bob Lewis is 12,300 $</p>', $output);
+    }
 }
