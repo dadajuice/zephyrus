fc1ab2ffe4b785b15b4971c69a06d1b73b9db83a
David Tucker <davidt2003@msn.com>
2017-01-12 22:26:10 -0500

    Added tests for Cookie class

---
+++ tests/network/CookieTest.php
@@ -0,0 +1,42 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Network\Cookie;
+
+class CookieTest extends TestCase
+{
+    public function testSend()
+    {
+        $cookie = new Cookie('test', '12345');
+        $cookie->send();
+        self::assertEquals('12345', $_COOKIE['test']);
+        self::assertEquals('12345', $cookie->getValue());
+        self::assertEquals('test', $cookie->getName());
+        $_COOKIE = [];
+    }
+
+    public function testDestroy()
+    {
+        $cookie = new Cookie('bob', '12345');
+        $cookie->send();
+        self::assertEquals('12345', $_COOKIE['bob']);
+        $cookie->destroy();
+        self::assertFalse(isset($_COOKIE['bob']));
+        $_COOKIE = [];
+    }
+
+    public function testParameters()
+    {
+        $cookie = new Cookie('bob');
+        $cookie->setDomain('test.local');
+        $cookie->setHttpOnly(false);
+        $cookie->setIsValueUrlEncoded(true);
+        $cookie->setLifetime(Cookie::DURATION_FOREVER);
+        $cookie->setPath('/users');
+        $cookie->setSecure(false);
+        $cookie->setValue('testing');
+        $cookie->send();
+        self::assertEquals('testing', $_COOKIE['bob']);
+        $_COOKIE = [];
+    }
+}
