704e4aa58f0f2011fce973a8540b112162d2c871
David Tucker <davidt2003@msn.com>
2017-01-29 17:46:08 -0500

    Added tests for CsrfGuard class

--- tests/security/CsrfGuardTest.php
+++ tests/security/CsrfGuardTest.php
@@ -1,6 +1,7 @@
 <?php namespace Zephyrus\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Zephyrus\Application\Session;
 use Zephyrus\Network\Request;
 use Zephyrus\Network\RequestFactory;
 use Zephyrus\Security\CsrfGuard;
@@ -9,14 +10,41 @@ class CsrfGuardTest extends TestCase
 {
     public function testHiddenFields()
     {
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
         $result = $csrf->generateHiddenFields();
         self::assertTrue($this->hasHiddenFields($result));
     }
 
+    public function testGuard()
+    {
+        $req = new Request('http://test.local/test', 'GET');
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
+        $csrf->setDeleteSecured(true);
+        $csrf->setGetSecured(false);
+        $csrf->setPostSecured(false);
+        $csrf->setPutSecured(false);
+        $csrf->guard();
+        $output = $csrf->generateHiddenFields();
+        $fields = $this->getHiddenFieldValues($output);
+        $name = $fields[1];
+        $value = $fields[2];
+
+        $req = new Request('http://test.local/test', 'DELETE', ['CSRFName' => $name, 'CSRFToken' => $value]);
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
+        $csrf->setDeleteSecured(true);
+        $csrf->setGetSecured(false);
+        $csrf->setPostSecured(false);
+        $csrf->setPutSecured(false);
+        $csrf->guard();
+        $test = "success";
+        self::assertEquals("success", $test);
+    }
+
     public function testFormInject()
     {
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
         $html = '<html><body><form action="test" method="get"><input type="text" name="test" /></form></body>';
         $result = $csrf->injectForms($html);
         self::assertTrue($this->hasHiddenFields($result));
@@ -24,7 +52,7 @@ class CsrfGuardTest extends TestCase
 
     public function testFormInjectExclusion()
     {
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
         $html = '<html><body><form nocsrf="true" action="test" method="get"><input type="text" name="test" /></form></body>';
         $result = $csrf->injectForms($html);
         self::assertEquals($html, $result);
@@ -32,7 +60,7 @@ class CsrfGuardTest extends TestCase
 
     public function testProperties()
     {
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
         $csrf->setDeleteSecured(true);
         $csrf->setPostSecured(true);
         $csrf->setPutSecured(true);
@@ -46,12 +74,22 @@ class CsrfGuardTest extends TestCase
     /**
      * @expectedException \Zephyrus\Exceptions\InvalidCsrfException
      */
-    public function testGuard()
+    public function testGuardException()
     {
-        CsrfGuard::kill();
         $req = new Request('http://test.local/test', 'POST');
         RequestFactory::set($req);
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
+        $csrf->guard();
+    }
+
+    /**
+     * @expectedException \Zephyrus\Exceptions\InvalidCsrfException
+     */
+    public function testInvalidToken()
+    {
+        $req = new Request('http://test.local/test', 'PUT', ['CSRFName' => 'invalid', 'CSRFToken' => 'invalid']);
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
         $csrf->guard();
     }
 
@@ -60,15 +98,56 @@ class CsrfGuardTest extends TestCase
      */
     public function testInvalidGuard()
     {
-        CsrfGuard::kill();
         $req = new Request('http://test.local/test', 'POST', ['CSRFName' => 'invalid', 'CSRFToken' => 'invalid']);
         RequestFactory::set($req);
-        $csrf = CsrfGuard::getInstance();
+        $csrf = new CsrfGuard();
+        $csrf->guard();
+    }
+
+    /**
+     * @expectedException \Zephyrus\Exceptions\InvalidCsrfException
+     */
+    public function testInvalidGetGuard()
+    {
+        $req = new Request('http://test.local/test', 'GET');
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
+        $csrf->setGetSecured(true);
+        $csrf->guard();
+    }
+
+    /**
+     * @expectedException \Zephyrus\Exceptions\InvalidCsrfException
+     */
+    public function testNoStorageGuard()
+    {
+        $req = new Request('http://test.local/test', 'GET');
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
+        $csrf->setGetSecured(false);
+        $csrf->guard();
+        $output = $csrf->generateHiddenFields();
+        $fields = $this->getHiddenFieldValues($output);
+        $name = $fields[1];
+        $value = $fields[2];
+
+        $req = new Request('http://test.local/test', 'DELETE', ['CSRFName' => $name, 'CSRFToken' => $value]);
+        RequestFactory::set($req);
+        $csrf = new CsrfGuard();
+        $csrf->setDeleteSecured(true);
+        Session::getInstance()->remove('__CSRF_TOKEN');
         $csrf->guard();
     }
 
     private function hasHiddenFields($html): bool
     {
         return (bool) preg_match("/<input type=\"hidden\" name=\"CSRFName\" value=\"CSRFGuard_[0-9]+\" \/><input type=\"hidden\" name=\"CSRFToken\" value=\"[0-9a-zA-Z]+\" \/>/", $html);
+    }
+
+    private function getHiddenFieldValues($html): array
+    {
+        $output = [];
+        preg_match("/<input type=\"hidden\" name=\"CSRFName\" value=\"(CSRFGuard_[0-9]+)\" \/><input type=\"hidden\" name=\"CSRFToken\" value=\"([0-9a-zA-Z]+)\" \/>/", $html, $output);
+        return $output;
     }
 }
