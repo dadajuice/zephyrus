fe6741cba1aa4b073a98beb2643de2eac04c33ef
David Tucker <davidt2003@msn.com>
2017-01-28 21:41:47 -0500

    Added tests for secure session storage class

--- tests/security/SecureSessionStorageTest.php
+++ tests/security/SecureSessionStorageTest.php
@@ -40,9 +40,159 @@ class SecureSessionStorageTest extends TestCase
         $req = new Request('http://test.local', 'GET', [], [], [], $server);
         RequestFactory::set($req);
         $storage = new SessionStorage($config, $req);
+        $storage->getFingerprint()->setIpAddressFingerprinted(true);
+        $storage->getFingerprint()->setUserAgentFingerprinted(true);
         $storage->start();
-        $_SESSION['test'] = '1234';
+        self::assertTrue($storage->getFingerprint()->isIpAddressFingerprinted());
+        self::assertTrue($storage->getFingerprint()->isUserAgentFingerprinted());
         $storage->destroy();
+    }
+
+    public function testIntervalExpiration()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_after_interval' => 2
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->getExpiration()->setRefreshAfterInterval(2);
+        $storage->start();
+        self::assertEquals(2, $storage->getExpiration()->getRefreshAfterInterval());
+        self::assertTrue(key_exists('__HANDLER_SECONDS_BEFORE_REFRESH', $_SESSION));
+        $storage->destroy();
+    }
+
+    public function testRequestExpiration()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_after_requests' => 3
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->getExpiration()->setRefreshAfterNthRequests(3);
+        $storage->start();
+        self::assertEquals(3, $storage->getExpiration()->getRefreshAfterNthRequests());
+        self::assertTrue(key_exists('__HANDLER_REQUESTS_BEFORE_REFRESH', $_SESSION));
+        $storage->destroy();
+    }
+
+    public function testProbabilityExpiration()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_probability' => 0.5
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->getExpiration()->setRefreshProbability(0.5);
+        $storage->start();
+        self::assertEquals(0.5, $storage->getExpiration()->getRefreshProbability());
+        $storage->destroy();
+    }
+
+    public function testExpirationCleanup()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_after_requests' => 3,
+            'refresh_after_interval' => 2
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->start();
+        self::assertTrue(key_exists('__HANDLER_REQUESTS_BEFORE_REFRESH', $_SESSION));
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false
+        ];
+        $storage = new SessionStorage($config, $req);
+        $storage->start();
+        self::assertFalse(key_exists('__HANDLER_REQUESTS_BEFORE_REFRESH', $_SESSION));
+        $storage->destroy();
+    }
+
+    public function testObsoleteByDirectProbability()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_probability' => 1
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->start();
+        $oldId = $storage->getId();
+        $storage->start();
+        $newId = $storage->getId();
+        self::assertNotEquals($newId, $oldId);
+        $storage->destroy();
+    }
+
+    public function testObsoleteByRequest()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_after_requests' => 2,
+            'refresh_probability' => 0.0
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->start();
+        $oldId = $storage->getId();
+        $storage->start();
+        $newId = $storage->getId();
+        self::assertEquals($newId, $oldId);
+        $storage->start();
+        $newId = $storage->getId();
+        self::assertNotEquals($newId, $oldId);
+        $storage->destroy();
+    }
+
+    public function testObsoleteByTimer()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_after_interval' => 1
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        $storage = new SessionStorage($config, $req);
+        $storage->start();
+        $oldId = $storage->getId();
+        sleep(1);
+        $storage->start();
+        $newId = $storage->getId();
+        self::assertNotEquals($newId, $oldId);
+        $storage->destroy();
+    }
+
+    /**
+     * @expectedException \RangeException
+     */
+    public function testInvalidProbability()
+    {
+        $config = [
+            'name' => 'bob',
+            'encryption_enabled' => false,
+            'refresh_probability' => 3
+        ];
+        $req = new Request('http://test.local', 'GET');
+        RequestFactory::set($req);
+        new SessionStorage($config, $req);
     }
 
     public function testDecoy()
