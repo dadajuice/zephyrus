277ed806e9b3fcd3d3004fc3c4b392b2a76ac007
David Tucker <davidt2003@msn.com>
2017-04-06 14:41:08 -0400

    Corrected bug in Validator interface which was not correctly linked

--- src/Zephyrus/Utilities/Validator.php
+++ src/Zephyrus/Utilities/Validator.php
@@ -5,9 +5,9 @@ interface Validator
     const PASSWORD_COMPLIANT = ['Zephyrus\Utilities\Validations\BaseValidation', 'isPasswordCompliant'];
     const NOT_EMPTY = ['Zephyrus\Utilities\Validations\BaseValidation', 'isNotEmpty'];
     const DECIMAL = ['Zephyrus\Utilities\Validations\NumericValidation', 'isDecimal'];
-    const DECIMAL_SIGNED = ['Zephyrus\Utilities\NumericValidation', 'isSignedDecimal'];
-    const INTEGER = ['Zephyrus\Utilities\NumericValidation', 'isInteger'];
-    const INTEGER_SIGNED = ['Zephyrus\Utilities\NumericValidation', 'isSignedInteger'];
+    const DECIMAL_SIGNED = ['Zephyrus\Utilities\Validations\NumericValidation', 'isSignedDecimal'];
+    const INTEGER = ['Zephyrus\Utilities\Validations\NumericValidation', 'isInteger'];
+    const INTEGER_SIGNED = ['Zephyrus\Utilities\Validations\NumericValidation', 'isSignedInteger'];
     const EMAIL = ['Zephyrus\Utilities\Validations\BaseValidation', 'isEmail'];
     const DATE_ISO = ['Zephyrus\Utilities\Validations\BaseValidation', 'isDate'];
     const ALPHANUMERIC = ['Zephyrus\Utilities\Validations\BaseValidation', 'isAlphanumeric'];
