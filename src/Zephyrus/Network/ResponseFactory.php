3b3a9c0d581f046c9dc9792278b784d35bf2c905
David Tucker <davidt2003@msn.com>
2017-12-25 21:31:59 -0500

    Added new response type to handle streaming SSE

--- src/Zephyrus/Network/ResponseFactory.php
+++ src/Zephyrus/Network/ResponseFactory.php
@@ -1,5 +1,6 @@
 <?php namespace Zephyrus\Network;
 
+use Zephyrus\Application\Callback;
 use Zephyrus\Application\Form;
 use Zephyrus\Application\ViewBuilder;
 
@@ -44,17 +45,75 @@ class ResponseFactory
         return $response;
     }
 
-    public function buildSse($data, $eventId = 0, $retry = 1000): Response
+    public function buildPollingSse($data, $eventName = 'stream', $retry = 1000): Response
     {
         $response = new Response(ContentType::SSE);
         $response->addHeader('Cache-Control', 'no-cache');
         ob_start();
-        echo "id: $eventId" . PHP_EOL;
+        echo "event: $eventName" . PHP_EOL;
         echo "retry: " . $retry . PHP_EOL;
-        echo "data: " . json_encode($data) . PHP_EOL;
+        echo "data: " . json_encode($data, JSON_FORCE_OBJECT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS) . PHP_EOL;
         echo PHP_EOL;
         $response->setContent(ob_get_clean());
         return $response;
+    }
+
+    public function buildStreamingSse($callback, $eventName, $sleep = 1): Response
+    {
+        // Make session readonly to prevent hang from other pages
+        // session_start(); is optional since its always starting from kernel
+        session_write_close();
+
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        header("Connection: keep-alive");
+
+        set_time_limit( 0 );
+        ignore_user_abort(true);
+        ini_set('auto_detect_line_endings', 1);
+        ini_set('max_execution_time', '0');
+        ob_end_clean();
+        gc_enable();
+
+        $callbackExecutionCounter = 1;
+        $call = new Callback($callback);
+
+        while (true) {
+            if (connection_status() != CONNECTION_NORMAL || connection_aborted()) {
+                break;
+            }
+
+            $data = $call->execute();
+            if (!empty($data)) {
+                $this->buildPollingSse($data, $eventName, $sleep * 1000)->sendContent();
+            }
+
+            if (@ob_get_level() > 0) {
+                for ($i = 0; $i < @ob_get_level(); $i++) {
+                    @ob_flush();
+                }
+            }
+            @flush();
+
+            sleep($sleep);
+            $callbackExecutionCounter++;
+
+            // Reduce potential memory leaks
+            // @see https://stackoverflow.com/questions/29480791/while-loops-for-server-sent-events-are-causing-page-to-freeze
+            if ($callbackExecutionCounter % 1000 == 0) {
+                gc_collect_cycles();
+                $callbackExecutionCounter = 1;
+            }
+        }
+
+        if (@ob_get_level() > 0) {
+            for ($i = 0; $i < @ob_get_level(); $i++) {
+                @ob_flush();
+            }
+            @ob_end_clean();
+        }
+
+        return new Response(ContentType::SSE);
     }
 
     public function buildRedirect(string $url): Response
