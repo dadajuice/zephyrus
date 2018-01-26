3f49d0cfad47fbe23d3d3c73e7701fa4f9d1357e
David Tucker <davidt2003@msn.com>
2018-01-25 23:47:09 -0500

    Added unit tests for ImageUploader class

---
+++ tests/utilities/ImageUploaderTest.php
@@ -0,0 +1,68 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Utilities\Uploaders\ImageUploader;
+use Zephyrus\Utilities\Uploaders\UploadFile;
+
+class ImageUploaderTest extends TestCase
+{
+    public function testDefaultConfiguration()
+    {
+        $file = $this->buildWorkingUploadFile();
+        $uploader = new ImageUploader($file);
+        self::assertTrue(in_array('gif', $uploader->getAllowedExtensions()));
+        self::assertTrue(in_array('image/gif', $uploader->getAllowedMimeTypes()));
+        self::assertFalse($uploader->isForcingImageRebuild());
+        $uploader->setForcingImageRebuild(true);
+        self::assertTrue($uploader->isForcingImageRebuild());
+    }
+
+    public function testValidImage()
+    {
+        $file = $this->buildWorkingUploadFile();
+        $uploader = new ImageUploader($file);
+        $uploader->setForcingImageRebuild(true);
+        $uploader->setDestinationDirectory(ROOT_DIR . '/lib');
+        try {
+            $uploader->upload();
+            self::assertEquals("1", "2");
+        } catch (\Exception $e) {
+            self::assertEquals("Upload failed", $e->getMessage());
+        }
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testInvalidImage()
+    {
+        $file = $this->buildInvalidUploadFile();
+        $uploader = new ImageUploader($file);
+        $uploader->setDestinationDirectory(ROOT_DIR . '/lib');
+        $uploader->upload();
+    }
+
+    private function buildWorkingUploadFile()
+    {
+        $data = [
+            'error' => UPLOAD_ERR_OK,
+            'type' => 'image/png',
+            'name' => 'working.png',
+            'tmp_name' => ROOT_DIR . '/lib/images/working.png',
+            'size' => 1300000
+        ];
+        return new UploadFile($data);
+    }
+
+    private function buildInvalidUploadFile()
+    {
+        $data = [
+            'error' => UPLOAD_ERR_OK,
+            'type' => 'image/png',
+            'name' => 'invalid.png',
+            'tmp_name' => ROOT_DIR . '/lib/images/invalid.png',
+            'size' => 1300000
+        ];
+        return new UploadFile($data);
+    }
+}
