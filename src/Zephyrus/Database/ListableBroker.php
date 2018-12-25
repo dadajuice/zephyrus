53c384868676079ec3a2c5947bc66414a192d533
David Tucker <davidt2003@msn.com>
2018-12-25 17:03:34 -0500

    Added new listableBroker class

---
+++ src/Zephyrus/Database/ListableBroker.php
@@ -0,0 +1,162 @@
+<?php namespace Zephyrus\Database;
+
+use Zephyrus\Network\RequestFactory;
+use Zephyrus\Utilities\Filter;
+
+abstract class ListableBroker extends Broker
+{
+    /**
+     * @var Filter
+     */
+    private $filter = null;
+
+    /**
+     * Retrieves the total number of available rows for the corresponding
+     * findAll method while applying the filters (sort, order and
+     * search). The filteredSelectSingle method should be used. E.g.:
+     *
+     *     return $this->filteredSelectSingle("SELECT COUNT(*) as n FROM user")->n;
+     *
+     * @return int
+     */
+    abstract public function count(): int;
+
+    /**
+     * Retrieves all the rows while applying the filters (sort, order and
+     * search). The filteredSelect method should be used. E.g.:
+     *
+     *     return $this->filteredSelect("SELECT * FROM user");
+     *
+     * @return int
+     */
+    abstract public function findAll(): array;
+
+    /**
+     * Must provide the search query respecting the established convention by
+     * using « :search » to identify search values. E.g.:
+     *
+     *     return "(username LIKE :search OR email LIKE :search OR CONCAT(firstname, ' ', lastname) LIKE :search)";
+     *
+     * The above example would allow search on the username, email and name
+     * fields.
+     *
+     * @return string
+     */
+    abstract protected function search(): string;
+
+    /**
+     * Must provide correspondence between sort label used on the front end (as
+     * table header links) and database columns. If the sort label and column
+     * are the same, its not necessary to include them in the resulting array.
+     * Only the different ones must be identified (key = sort label,
+     * value = sort column). E.g.:
+     *
+     *     return [
+     *         'name' => 'firstname $order, lastname',
+     *         'login' => 'last_login'
+     *     ];
+     *
+     * This method is forced as abstract because it is not a good behavior to
+     * expose database columns as sort labels and also gives opportunity to
+     * properly translate the sort labels if needed.
+     *
+     * @param string $order
+     * @return string[]
+     */
+    abstract function sort(string $order): array;
+
+    /**
+     * @return null | Filter
+     */
+    public function getFilter(): ?Filter
+    {
+        return $this->filter;
+    }
+
+    /**
+     * Creates the filter according to the request.
+     *
+     * @param string $defaultSort
+     */
+    public function applyFilter(string $defaultSort = "")
+    {
+        $this->filter = new Filter(RequestFactory::read(), $defaultSort);
+    }
+
+    public function removeFilter()
+    {
+        $this->filter = null;
+    }
+
+    /**
+     * @param string $query
+     * @param array $parameters
+     * @param string $allowedTags
+     * @return \stdClass | null
+     */
+    protected function filteredSelectSingle(string $query, array $parameters = [], string $allowedTags = ""): ?\stdClass
+    {
+        return $this->filteredQuery(true, $query, $parameters, $allowedTags);
+    }
+
+    /**
+     * @param string $query
+     * @param array $parameters
+     * @param string $allowedTags
+     * @return \stdClass[]
+     */
+    protected function filteredSelect(string $query, array $parameters = [], string $allowedTags = ""): array
+    {
+        return $this->filteredQuery(false, $query, $parameters, $allowedTags);
+    }
+
+    private function filteredQuery(bool $single, string $query, array $parameters = [], string $allowedTags = "")
+    {
+        if (!is_null($this->filter)) {
+            $search = $this->filter->getSearch();
+            if (!is_null($search)) {
+                $search = "%$search%";
+                $query = $this->buildSearch($query);
+            }
+            $query .= $this->buildOrderBy();
+            $parameters = array_merge($parameters, ['search' => $search]);
+        }
+        return ($single)
+            ? $this->selectSingle($query, $parameters, $allowedTags)
+            : $this->select($query, $parameters, $allowedTags);
+    }
+
+    private function buildSearch(string $query)
+    {
+        $lastWhereByOccurrence = strripos($query, "where");
+        $lastGroupByOccurrence = strripos($query, "group by");
+        $lastHavingByOccurrence = strripos($query, "having");
+        $insertionPosition = strlen($query);
+        if ($lastGroupByOccurrence !== false && $lastGroupByOccurrence > $lastWhereByOccurrence) {
+            $insertionPosition = $lastGroupByOccurrence;
+        } elseif ($lastHavingByOccurrence !== false && $lastHavingByOccurrence > $lastWhereByOccurrence) {
+            $insertionPosition = $lastHavingByOccurrence;
+        }
+        $begin = substr($query, 0, $insertionPosition);
+        $end = substr($query, $insertionPosition);
+        return $begin . $this->buildWhere($query) . $end;
+    }
+
+    private function buildWhere(string $query): string
+    {
+        $lastWhereByOccurrence = strripos($query, "where");
+        return (($lastWhereByOccurrence !== false) ? " AND " : " WHERE ") . $this->search();
+    }
+
+    private function buildOrderBy(): string
+    {
+        $sort = $this->filter->getSort();
+        if (!empty($sort)) {
+            $order = $this->filter->getOrder();
+            $convertedSorts = $this->sort($order);
+            $orderBy = (key_exists($sort, $convertedSorts)) ? $convertedSorts[$sort] : $sort;
+            return " ORDER BY $orderBy $order";
+        }
+        return "";
+    }
+}
---
+++ tests/database/ListableBrokerTest.php
@@ -0,0 +1,83 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Database\Database;
+use Zephyrus\Database\ListableBroker;
+use Zephyrus\Network\Request;
+use Zephyrus\Network\RequestFactory;
+
+class ListableBrokerTest extends TestCase
+{
+    /**
+     * @var Database
+     */
+    private static $database;
+
+    public static function setUpBeforeClass()
+    {
+        self::$database = new Database('sqlite::memory:');
+        self::$database->query('CREATE TABLE heroes(id NUMERIC PRIMARY KEY, name TEXT);');
+        self::$database->query("INSERT INTO heroes(id, name) VALUES (1, 'Batman');");
+        self::$database->query("INSERT INTO heroes(id, name) VALUES (2, 'Superman');");
+        self::$database->query("INSERT INTO heroes(id, name) VALUES (3, 'Aquaman');");
+        self::$database->query("INSERT INTO heroes(id, name) VALUES (4, 'Flash');");
+    }
+
+    public function testFindAll()
+    {
+        $r = new Request('http://test.local', 'GET', ['parameters' => ['search' => 'man', 'order' => 'asc', 'sort' => 'alias']]);
+        RequestFactory::set($r);
+        $class = $this->buildClass();
+        $class->applyFilter();
+        self::assertEquals('man', $class->getFilter()->getSearch());
+        self::assertEquals('Aquaman', $class->findAll()[0]->name);
+    }
+
+    public function testRemoveFilter()
+    {
+        $r = new Request('http://test.local', 'GET', ['parameters' => ['search' => 'man', 'order' => 'asc', 'sort' => 'alias']]);
+        RequestFactory::set($r);
+        $class = $this->buildClass();
+        $class->applyFilter();
+        self::assertEquals('Aquaman', $class->findAll()[0]->name);
+        $class->removeFilter();
+        self::assertEquals('Batman', $class->findAll()[0]->name);
+    }
+
+    public function testCount()
+    {
+        $r = new Request('http://test.local', 'GET', ['parameters' => ['search' => 'man']]);
+        RequestFactory::set($r);
+        $class = $this->buildClass();
+        self::assertEquals(4, $class->count());
+        $class->applyFilter();
+        self::assertEquals(3, $class->count());
+    }
+
+    private function buildClass()
+    {
+        return new class(self::$database) extends ListableBroker {
+            public function count(): int
+            {
+                return $this->filteredSelectSingle("SELECT COUNT(*) as n FROM heroes")->n;
+            }
+
+            public function findAll(): array
+            {
+                return $this->filteredSelect("SELECT * FROM heroes");
+            }
+
+            protected function search(): string
+            {
+                return "(name LIKE :search)";
+            }
+
+            function sort(string $order): array
+            {
+                return [
+                    'alias' => 'name'
+                ];
+            }
+        };
+    }
+}
