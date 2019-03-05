7ebff7c8246491105bf162b431a5df864f3e183d
David Tucker <davidt2003@msn.com>
2019-03-05 10:10:33 -0500

    Refactoring for styleCI compliance

--- src/Zephyrus/Database/Listable.php
+++ src/Zephyrus/Database/Listable.php
@@ -6,8 +6,12 @@ use Zephyrus\Utilities\Pager;
 interface Listable
 {
     public function findAll(): array;
+
     public function count(): int;
+
     public function getFilter(): ?Filter;
+
     public function applyFilter(string $defaultSort = "", string $defaultOrder = "");
+
     public function buildPager($count, $limit = Pager::PAGE_MAX_ENTITIES, $urlParameter = Pager::URL_PARAMETER);
 }
