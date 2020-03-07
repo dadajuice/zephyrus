891d6c3d797ec1a5bd6b162f2d72ae4bda6fa7da
David Tucker <davidt2003@msn.com>
2020-03-07 14:40:00 -0500

    Added test for HttpRequester class

--- tests/network/HttpRequesterTest.php
+++ tests/network/HttpRequesterTest.php
@@ -1,6 +1,7 @@
 <?php namespace Zephyrus\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Zephyrus\Network\ContentType;
 use Zephyrus\Network\HttpRequester;
 use Zephyrus\Utilities\FileSystem\File;
 
@@ -13,10 +14,26 @@ class HttpRequesterTest extends TestCase
         $request = HttpRequester::delete("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/existing.txt");
         $request = HttpRequester::patch("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/existing.txt");
         $request = HttpRequester::get("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/existing.txt");
+        $request->setConnectionTimeout(10);
+        $request->setContentType(ContentType::PLAIN);
+        $request->setFollowRedirection(true);
+        $request->setUserAgent("PHPUnit Test");
         $request->execute(['t' => time()]);
         self::assertEquals('200', $request->getResponseHttpCode());
         self::assertEquals('200', $request->getResponseInfo()['http_code']);
         self::assertEquals('One ring to rule them all', $request->getResponse());
+
+        $request = HttpRequester::put("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/existing.txt");
+        $request->addHeaders(['X-APP' => 'PHPUnit']);
+        $request->addOptions([CURLOPT_RETURNTRANSFER => true]);
+        $request->execute();
+
+        $request = HttpRequester::get("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/existing.txt");
+
+        $request->executeStream(function ($result, $info) {
+            self::assertEquals('One ring to rule them all', $result);
+        });
+
     }
 
     /**
@@ -45,6 +62,14 @@ class HttpRequesterTest extends TestCase
     {
         $request = HttpRequester::post("https://raw.githubusercontent.com/dadajuice/zephyrus/master/tests/lib/filesystem/sdfdgdfdgfdfg.txt");
         $file = HttpRequester::prepareUploadFile(ROOT_DIR . '/lib/filesystem/existing.txt', 'test.txt');
-        $request->execute(['file' => $file]);
+        $request->execute(['file' => $file, 'test' => ['name' => 't', 'age' => 3, 'classes' => ['nest', 'nest 2']]]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidUpload()
+    {
+        $file = HttpRequester::prepareUploadFile(ROOT_DIR . '/lib/filesystem/existidsfsdfdsfdsdfdsfsdfsdfng.txt', 'test.txt');
     }
 }
