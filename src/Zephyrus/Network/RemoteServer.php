b55d65b55e8d41c71e6b3b730af4790993cd7d3c
David Tucker <davidt2003@msn.com>
2020-03-08 17:40:13 -0400

    Added RemoteServer class

---
+++ src/Zephyrus/Network/RemoteServer.php
@@ -0,0 +1,82 @@
+<?php namespace Zephyrus\Network;
+
+use Zephyrus\Database\Broker;
+
+class RemoteServer
+{
+    const DEFAULT_PING_TIMEOUT = 2;
+
+    /**
+     * @var string
+     */
+    private $hostname;
+
+    /**
+     * @var string
+     */
+    private $ipAddress;
+
+    public function __construct(string $hostname)
+    {
+        if (filter_var($hostname, FILTER_VALIDATE_IP)) {
+            $this->ipAddress = $hostname;
+            $this->hostname = null;
+        } else {
+            $this->hostname = $hostname;
+            $this->ipAddress = gethostbyname($hostname);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostname(): ?string
+    {
+        return $this->hostname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress(): string
+    {
+        return $this->ipAddress;
+    }
+
+    // DNS_A, DNS_CNAME, DNS_HINFO, DNS_CAA, DNS_MX, DNS_NS, DNS_PTR, DNS_SOA, DNS_TXT, DNS_AAAA, DNS_SRV, DNS_NAPTR, DNS_A6, DNS_ALL or DNS_ANY.
+    public function getDnsRecord(int $type = DNS_ANY): array
+    {
+        return dns_get_record($this->ipAddress, $type);
+    }
+
+    /**
+     * Tries to open a socket within the given timeout at the specified port.
+     *
+     * @param int $port
+     * @param int $timeout
+     * @return bool
+     */
+    public function isServiceAvailable(int $port = 80, int $timeout = self::DEFAULT_PING_TIMEOUT): bool
+    {
+        $isUp = false;
+        if ($socket =@ fsockopen($this->ipAddress, $port, $errno, $errstr, $timeout)) {
+            $isUp = true;
+            fclose($socket);
+        }
+        return $isUp;
+    }
+
+    /**
+     * Retrieves the ISO date of the SSL certificate.
+     *
+     * @param int $port
+     * @return string
+     */
+    public function getSslExpiration(int $port = 443): string
+    {
+        $results = shell_exec("echo | openssl s_client -connect {$this->ipAddress}:$port 2>/dev/null | openssl x509 -noout -dates");
+        $parts = array_filter(explode(PHP_EOL, $results));
+        $expiration = str_replace('notAfter=', '', $parts[1]);
+        return date(Broker::SQL_FORMAT_DATE_TIME, strtotime($expiration));
+    }
+}
---
+++ tests/network/RemoteServerTest.php
@@ -0,0 +1,28 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Network\RemoteServer;
+use Zephyrus\Utilities\Validation;
+
+class RemoteServerTest extends TestCase
+{
+    public function testAvailable()
+    {
+        $remote = new RemoteServer("github.com");
+        self::assertTrue($remote->isServiceAvailable(443));
+    }
+
+    public function testDns()
+    {
+        $remote = new RemoteServer("cegepst.qc.ca");
+        self::assertTrue(!empty($remote->getDnsRecord(DNS_A)[0]['host']));
+        self::assertEquals('cegepst.qc.ca', $remote->getHostname());
+        self::assertTrue(Validation::isIPv4($remote->getIpAddress()));
+    }
+
+    public function testDns2()
+    {
+        $remote = new RemoteServer("206.167.240.12");
+        self::assertEquals(null, $remote->getHostname());
+    }
+}
