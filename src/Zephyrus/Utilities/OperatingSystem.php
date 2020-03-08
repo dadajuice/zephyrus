3963b6b91d39574d280885f3d354d8e7af876229
David Tucker <davidt2003@msn.com>
2020-03-08 16:58:11 -0400

    Added OperatingSystem class methods

--- src/Zephyrus/Utilities/OperatingSystem.php
+++ src/Zephyrus/Utilities/OperatingSystem.php
@@ -4,7 +4,7 @@ use stdClass;
 
 class OperatingSystem
 {
-    public static function diskStats(string $partitionPath = "/var/www"): stdClass
+    public static function getDiskStats(string $partitionPath = "/var/www"): stdClass
     {
         $df = disk_free_space($partitionPath);
         $dt = disk_total_space($partitionPath);
@@ -16,15 +16,29 @@ class OperatingSystem
         ];
     }
 
-    public static function getName()
+    public static function getName(): stdClass
     {
+        return (object) [
+            'system' => php_uname("s"),
+            'release' => php_uname("r"),
+            'version' => php_uname("v"),
+            'machine' => php_uname("m")
+        ];
     }
 
-    public static function getCpu()
+    public static function getCpuAverageLoad()
     {
+        return sys_getloadavg()[0];
     }
 
-    public static function getMemory()
+    public static function getMemoryUsage()
     {
+        $free = shell_exec('free');
+        $free = (string)trim($free);
+        $free_arr = explode("\n", $free);
+        $mem = explode(" ", $free_arr[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        return ($mem[2] / $mem[1]);
     }
 }
---
+++ tests/utilities/OperatingSystemTest.php
@@ -0,0 +1,21 @@
+<?php namespace Zephyrus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Utilities\OperatingSystem;
+
+class OperatingSystemTest extends TestCase
+{
+    public function testName()
+    {
+        $results = OperatingSystem::getName();
+        self::assertTrue(is_object($results));
+    }
+
+    public function testDiskStats()
+    {
+        $results = OperatingSystem::getDiskStats();
+        self::assertTrue(is_object($results));
+        OperatingSystem::getCpuAverageLoad();
+        OperatingSystem::getMemoryUsage();
+    }
+}
