7664c8b6a4d44917c562ff129596b8882f3392dc
dadajuice <davidt2003@msn.com>
2020-08-05 09:27:53 -0400

    Added test case for parameter override

--- tests/application/ControllerOverrideTest.php
+++ tests/application/ControllerOverrideTest.php
@@ -37,4 +37,41 @@ class ControllerOverrideTest extends TestCase
         $output = ob_get_clean();
         self::assertEquals('msandwich44', $output);
     }
+
+    public function testOverrideNullParameters()
+    {
+        $router = new Router();
+        $controller = new class($router) extends Controller {
+
+            public function initializeRoutes()
+            {
+                parent::get('/users/{user}', 'read');
+
+                parent::overrideParameter('user', function ($value) {
+                    if ($value == 4) {
+                        return (object) [
+                            'id' => $value,
+                            'username' => 'msandwich'
+                        ];
+                    }
+                    return null;
+                });
+            }
+
+            public function read(?\stdClass $user)
+            {
+                if (is_null($user)) {
+                    return $this->plain("entity not found");
+                }
+                $id = $this->request->getParameter('user')->id; // request should be overridden too
+                return $this->plain($user->username . $id . $user->id);
+            }
+        };
+        $controller->initializeRoutes();
+        $req = new Request('http://test.local/users/9', 'get');
+        ob_start();
+        $router->run($req);
+        $output = ob_get_clean();
+        self::assertEquals('entity not found', $output);
+    }
 }
