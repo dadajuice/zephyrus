596c24c681b6d1f6a4d3b63a1afec565885e331a
David Tucker <davidt2003@msn.com>
2021-02-28 10:17:46 -0500

    Added method to encrypt and decrypt files

--- src/Zephyrus/Utilities/FileSystem/File.php
+++ src/Zephyrus/Utilities/FileSystem/File.php
@@ -1,5 +1,7 @@
 <?php namespace Zephyrus\Utilities\FileSystem;
 
+use Zephyrus\Security\Cryptography;
+
 class File extends FileSystemNode
 {
     /**
@@ -23,8 +25,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Retrieves the file extension (e.g. /var/www/project/martin.txt would
-     * return "txt").
+     * Retrieves the file extension (e.g. /var/www/project/martin.txt would return "txt").
      *
      * @return string
      */
@@ -34,8 +35,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Retrieves the name of the file without the extension (e.g
-     * /var/www/project/martin.txt would return "martin").
+     * Retrieves the name of the file without the extension (e.g /var/www/project/martin.txt would return "martin").
      *
      * @return string
      */
@@ -75,8 +75,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Retrieves the real file mime type (using finfo) and not considering
-     * the file extension.
+     * Retrieves the real file mime type (using finfo) and not considering the file extension.
      *
      * @return string
      */
@@ -89,8 +88,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Outputs the file content to the buffer. Equivalent of doing an "echo" of
-     * the entire data to the response.
+     * Outputs the file content to the buffer. Equivalent of doing an "echo" of the entire data to the response.
      */
     public function output()
     {
@@ -98,9 +96,8 @@ class File extends FileSystemNode
     }
 
     /**
-     * Returns the entire file content into a string. Should not be used with
-     * huge files. Consider using the "output" method for displaying very huge
-     * file content to the response buffer.
+     * Returns the entire file content into a string. Should not be used with huge files. Consider using the "output"
+     * method for displaying very huge file content to the response buffer.
      *
      * @return string
      */
@@ -110,8 +107,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Overwrites the file actual content with the given data. The data can be
-     * string, array or resource stream.
+     * Overwrites the file actual content with the given data. The data can be string, array or resource stream.
      *
      * @param mixed $data
      */
@@ -121,8 +117,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Appends the given data at the end of the actual file content. The data
-     * can be string, array or resource stream.
+     * Appends the given data at the end of the actual file content. The data can be string, array or resource stream.
      *
      * @param $data
      */
@@ -132,9 +127,48 @@ class File extends FileSystemNode
     }
 
     /**
-     * If the initial path is a single file, will remove it. If the given path
-     * is a directory, it will completely empty it and then remove the
-     * directory. Returns true on success and false on failure.
+     * Encrypts the content of the file and either save it directly or create a new file at the given $destination that
+     * will hold the ciphertext result. Useful if the original file needs to be kept intact somehow. Dont forget the
+     * key as it will be needed to decrypt the file.
+     *
+     * @param string $key
+     * @param string|null $destination
+     */
+    public function encrypt(string $key, ?string $destination = null)
+    {
+        $clearTextContent = $this->read();
+        $cipher = Cryptography::encrypt($clearTextContent, $key);
+        if (!is_null($destination)) {
+            $file = File::create($destination, true);
+            $file->write($cipher);
+        } else {
+            $this->write($cipher);
+        }
+    }
+
+    /**
+     * Decrypts the content of the file and either save it directly or create a new file at the given $destination that
+     * will hold the clear text result. Useful if the original file needs to stay encrypted. Needs the same key used
+     * to encrypt.
+     *
+     * @param string $key
+     * @param string|null $destination
+     */
+    public function decrypt(string $key, ?string $destination = null)
+    {
+        $cipher = $this->read();
+        $clearTextContent = Cryptography::decrypt($cipher, $key);
+        if (!is_null($destination)) {
+            $file = File::create($destination, true);
+            $file->write($clearTextContent);
+        } else {
+            $this->write($clearTextContent);
+        }
+    }
+
+    /**
+     * If the initial path is a single file, will remove it. If the given path is a directory, it will completely empty
+     * it and then remove the directory. Returns true on success and false on failure.
      */
     public function remove(): bool
     {
@@ -142,8 +176,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Returns the total file size of the specified directory or single file in
-     * bytes.
+     * Returns the total file size of the specified directory or single file in bytes.
      *
      * @return int
      */
@@ -153,9 +186,8 @@ class File extends FileSystemNode
     }
 
     /**
-     * Obtains the last modification timestamp of the path/file defined in the
-     * constructor. If its a directory, it will automatically fetch the latest
-     * modified time.
+     * Obtains the last modification timestamp of the path/file defined in the constructor. If its a directory, it will
+     * automatically fetch the latest modified time.
      *
      * @return int
      */
@@ -165,8 +197,7 @@ class File extends FileSystemNode
     }
 
     /**
-     * Updates the modification time of the file to the given timestamp or
-     * simply the current time if none is supplied.
+     * Updates the modification time of the file to the given timestamp or simply the current time if none is supplied.
      *
      * @param int|null $timestamp
      */
@@ -176,9 +207,8 @@ class File extends FileSystemNode
     }
 
     /**
-     * Opens the file in memory and returns the resource handle for manual
-     * manipulations of the file. Will need to be manually closed outside of
-     * this class. The available modes are the one used with fopen.
+     * Opens the file in memory and returns the resource handle for manual manipulations of the file. Will need to be
+     * manually closed outside of this class. The available modes are the one used with fopen.
      *
      * @see https://www.php.net/manual/en/function.fopen.php
      * @param string $mode
