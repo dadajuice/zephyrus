565e79f086618c4b16e0feca8e9d8c9c111638c6
dadajuice <davidt2003@msn.com>
2022-01-09 14:15:54 -0500

    Applied default ini_set settings to mimic .htaccess

--- tests/lib/init.php
+++ tests/lib/init.php
@@ -4,6 +4,10 @@ if (!$loader = @require __DIR__ . '/../../vendor/autoload.php') {
     die('You must set up the project dependencies, run composer install');
 }
 
+// Simulate .htaccess default settings
+ini_set('session.use_cookies', 1);
+ini_set('session.use_only_cookies', 1);
+
 define('ROOT_DIR', __DIR__ . '/..');
 $loader->addPsr4('Controllers\\', ROOT_DIR . '/app/Controllers');
 require_once ROOT_DIR . '/../src/Zephyrus/functions.php';
