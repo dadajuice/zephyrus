acfce3f018553d186c84df40a6a4ad4a169f09f3
dadajuice <davidt2003@msn.com>
2022-01-28 09:46:28 -0500

    Updated cookie related tests

--- tests/Network/CookieTest.php
+++ tests/Network/CookieTest.php
@@ -1,19 +1,48 @@
 <?php namespace Zephyrus\Tests\Network;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Zephyrus\Network\Cookie;
 
 class CookieTest extends TestCase
 {
-    public function testSend()
+    protected function tearDown(): void
     {
-        //upgrade with tests including xdebug_get_headers set-cookie
-        $cookie = new Cookie('test', '12345');
-        $cookie->send();
-        self::assertEquals('12345', $_COOKIE['test']);
-        self::assertEquals('12345', $cookie->getValue());
-        self::assertEquals('test', $cookie->getName());
         $_COOKIE = [];
+    }
+
+    public function testDefaultSend()
+    {
+        $cookie = new Cookie('test_default_cookie', '12345');
+        $cookie->send();
+        self::assertEquals('12345', $_COOKIE['test_default_cookie']);
+        self::assertEquals('12345', $cookie->getValue());
+        self::assertEquals('test_default_cookie', $cookie->getName());
+
+        $header = getSetCookieHeader("test_default_cookie");
+        self::assertNotNull($header);
+
+        $cookieContent = str_replace("Set-Cookie: ", "", $header);
+        $cookieSegment = explode("; ", $cookieContent);
+        self::assertEquals("test_default_cookie=12345", $cookieSegment[0]);
+        self::assertEquals("path=/", $cookieSegment[3]);
+        self::assertEquals("HttpOnly", $cookieSegment[4]);
+        self::assertEquals("SameSite=Strict", $cookieSegment[5]);
+    }
+
+    public function testStaticRead()
+    {
+        $cookie = new Cookie('heroes', 'Batman');
+        $cookie->send();
+        self::assertEquals("Batman", Cookie::read('heroes'));
+        self::assertEquals("Robin", Cookie::read('unavailable', 'Robin'));
+    }
+
+    public function testInvalidSameSite()
+    {
+        self::expectException(InvalidArgumentException::class);
+        $cookie = new Cookie('same_site', 'bad_value');
+        $cookie->setSameSite("invalid");
     }
 
     public function testDestroy()
@@ -23,12 +52,11 @@ class CookieTest extends TestCase
         self::assertEquals('12345', $_COOKIE['bob']);
         $cookie->destroy();
         self::assertFalse(isset($_COOKIE['bob']));
-        $_COOKIE = [];
     }
 
     public function testParameters()
     {
-        $cookie = new Cookie('bob');
+        $cookie = new Cookie('rolan');
         $cookie->setDomain('test.local');
         $cookie->setHttpOnly(false);
         $cookie->setIsValueUrlEncoded(true);
@@ -36,8 +64,18 @@ class CookieTest extends TestCase
         $cookie->setPath('/users');
         $cookie->setSecure(false);
         $cookie->setValue('testing');
+        $cookie->setSameSite("Lax");
         $cookie->send();
-        self::assertEquals('testing', $_COOKIE['bob']);
-        $_COOKIE = [];
+        self::assertEquals('testing', $_COOKIE['rolan']);
+
+        $header = getSetCookieHeader("rolan");
+        self::assertNotNull($header);
+
+        $cookieContent = str_replace("Set-Cookie: ", "", $header);
+        $cookieSegment = explode("; ", $cookieContent);
+        self::assertEquals("rolan=testing", $cookieSegment[0]);
+        self::assertEquals("path=/users", $cookieSegment[3]);
+        self::assertEquals("domain=test.local", $cookieSegment[4]);
+        self::assertEquals("SameSite=Lax", $cookieSegment[5]);
     }
 }
