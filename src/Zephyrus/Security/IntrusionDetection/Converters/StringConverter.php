8039b00c2a1baa3ea862af138dc2bab2a9b5d0c7
dadajuice <davidt2003@msn.com>
2022-02-02 13:28:39 -0500

    Corrected IDS false positive case

--- src/Zephyrus/Security/IntrusionDetection/Converters/StringConverter.php
+++ src/Zephyrus/Security/IntrusionDetection/Converters/StringConverter.php
@@ -94,7 +94,9 @@ trait StringConverter
     private function convertQuotes(string $value): string
     {
         // normalize different quotes to "
-        $pattern = ['\'', '`', '´', '’', '‘'];
+        // Original value : ['\'', '`', '´', '’', '‘']
+        // Removed "’" because it can be used in for legit apostrophe
+        $pattern = ['\'', '`', '´', '‘'];
         $value = str_replace($pattern, '"', $value);
         //make sure harmless quoted strings don't generate false alerts
         $value = preg_replace('/^"([^"=\\!><~]+)"$/', '$1', $value);
