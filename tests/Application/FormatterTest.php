32375ee2c775b36ae74c158c94e66fbce9a19e6a
Nicolas Lacombe <nicolasswtor135@hotmail.ca>
2022-02-28 15:03:55 -0500

    Fix formatting for default HOUR

--- tests/Application/FormatterTest.php
+++ tests/Application/FormatterTest.php
@@ -94,6 +94,9 @@ class FormatterTest extends TestCase
     {
         $result = Formatter::datetime('2016-01-01 23:15:00');
         self::assertEquals('1 janvier 2016, 23:15', $result);
+
+        $result = Formatter::datetime('2016-01-01 01:15:00');
+        self::assertEquals('1 janvier 2016, 01:15', $result);
     }
 
     public function testFormatSizeKb()
