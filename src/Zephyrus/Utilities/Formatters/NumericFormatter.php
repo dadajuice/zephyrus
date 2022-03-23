1af4c14670d25d31ca642fbbc534bf423b75c9ba
Nicolas Lacombe <nicolasswtor135@hotmail.ca>
2022-03-23 16:34:56 -0400

    Handling of null values in formatters and refactor

--- src/Zephyrus/Utilities/Formatters/NumericFormatter.php
+++ src/Zephyrus/Utilities/Formatters/NumericFormatter.php
@@ -4,36 +4,48 @@ use Zephyrus\Application\Configuration;
 
 trait NumericFormatter
 {
-    public static function percent($number, $minDecimals = 2, $maxDecimals = 4)
+    public static function percent(int|float|null $number, int $minDecimals = 2, int $maxDecimals = 4): string
     {
+        if (is_null($number)) {
+            return "-";
+        }
+
         $locale = Configuration::getApplicationConfiguration('locale');
         $formatter = new \NumberFormatter($locale, \NumberFormatter::PERCENT);
         $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
         $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
         $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
-        $result = $formatter->format($number, \NumberFormatter::TYPE_DOUBLE);
+        $result = $formatter->format($number, \NumberFormatter::TYPE_DOUBLE) ?: "-";
         return $result;
     }
 
-    public static function money($amount, $minDecimals = 2, $maxDecimals = 2)
+    public static function money(int|float|null $amount, int $minDecimals = 2, int $maxDecimals = 2): string
     {
+        if (is_null($amount)) {
+            return "-";
+        }
+
         $locale = Configuration::getApplicationConfiguration('locale');
         $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
         $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
         $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
         $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
-        $result = $formatter->formatCurrency($amount, Configuration::getApplicationConfiguration('currency'));
+        $result = $formatter->formatCurrency($amount, Configuration::getApplicationConfiguration('currency')) ?: "-";
         return $result;
     }
 
-    public static function decimal($number, $minDecimals = 2, $maxDecimals = 4)
+    public static function decimal(int|float|null $number, int $minDecimals = 2, int $maxDecimals = 4): string
     {
+        if (is_null($number)) {
+            return "-";
+        }
+
         $locale = Configuration::getApplicationConfiguration('locale');
         $formatter = new \NumberFormatter($locale, \NumberFormatter::DECIMAL);
         $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
         $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
         $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
-        $result = $formatter->format($number, \NumberFormatter::TYPE_DOUBLE);
+        $result = $formatter->format($number, \NumberFormatter::TYPE_DOUBLE) ?: "-";
         return $result;
     }
 }
