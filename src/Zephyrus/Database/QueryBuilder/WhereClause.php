a8a10e9e7e8b8fdc2f1a2140e32b345510f9df06
dadajuice <davidt2003@msn.com>
2022-04-05 09:26:57 -0400

    Added class to build database where clause

---
+++ src/Zephyrus/Database/QueryBuilder/WhereClause.php
@@ -0,0 +1,50 @@
+<?php namespace Zephyrus\Database\QueryBuilder;
+
+class WhereClause
+{
+    public const OPERATOR_AND = "AND";
+    public const OPERATOR_OR = "OR";
+    private const SUPPORTED_OPERATORS = [self::OPERATOR_AND, self::OPERATOR_OR];
+
+    private string $whereClause = "";
+    private array $queryParameters = [];
+
+    public function and(WhereCondition $condition): self
+    {
+        return $this->add($condition, self::OPERATOR_AND);
+    }
+
+    public function or(WhereCondition $condition): self
+    {
+        return $this->add($condition, self::OPERATOR_OR);
+    }
+
+    public function add(WhereCondition $condition, string $logicalOperator): self
+    {
+        $logicalOperator = strtoupper($logicalOperator);
+        if (!in_array($logicalOperator, self::SUPPORTED_OPERATORS)) {
+            throw new \InvalidArgumentException("Logical operator must be either AND or OR.");
+        }
+        if (!empty($this->whereClause)) {
+            $this->whereClause .= " $logicalOperator ";
+        }
+        $this->concatCondition($condition);
+        return $this;
+    }
+
+    public function getQueryParameters(): array
+    {
+        return $this->queryParameters;
+    }
+
+    public function getSql(): string
+    {
+        return ((empty($this->whereClause)) ?: 'WHERE ') . $this->whereClause;
+    }
+
+    private function concatCondition(WhereCondition $baseCondition)
+    {
+        $this->queryParameters = array_merge($this->queryParameters, $baseCondition->getQueryParameters());
+        $this->whereClause .= $baseCondition->getSql();
+    }
+}
