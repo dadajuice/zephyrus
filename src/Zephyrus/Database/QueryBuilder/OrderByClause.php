8c4581561b5016010da05af5dc4e6f58bb884d43
dadajuice <davidt2003@msn.com>
2022-04-05 09:26:39 -0400

    Added class to build database order by clause

---
+++ src/Zephyrus/Database/QueryBuilder/OrderByClause.php
@@ -0,0 +1,36 @@
+<?php namespace Zephyrus\Database\QueryBuilder;
+
+class OrderByClause
+{
+    private string $orderBySql;
+
+    public function __construct()
+    {
+        $this->orderBySql = "";
+    }
+
+    public function getSql(): string
+    {
+        return ((empty($this->orderBySql)) ?: 'ORDER BY ') . $this->orderBySql;
+    }
+
+    public function asc(string $column, bool $nullLast = true): self
+    {
+        $this->addOrderClause("$column ASC" . ($nullLast ? '' : " NULLS FIRST"));
+        return $this;
+    }
+
+    public function desc(string $column, bool $nullFirst = true): self
+    {
+        $this->addOrderClause("$column DESC" . ($nullFirst ? '' : " NULLS LAST"));
+        return $this;
+    }
+
+    private function addOrderClause(string $clause)
+    {
+        if (!empty($this->orderBySql)) {
+            $this->orderBySql .= ', ';
+        }
+        $this->orderBySql .= $clause;
+    }
+}
