183152740520e4b570fd446aaeb5bb705e9b916b
dadajuice <davidt2003@msn.com>
2022-04-05 09:48:34 -0400

    Refactored database connector class

--- src/Zephyrus/Database/Core/DatabaseConnector.php
+++ src/Zephyrus/Database/Core/DatabaseConnector.php
@@ -1,29 +1,35 @@
 <?php namespace Zephyrus\Database\Core;
 
-class DatabaseConnector extends \PDO
+use PDO;
+
+class DatabaseConnector extends PDO
 {
     private static array $savepointEnabled = ["pgsql", "mysql", "sqlite"];
 
     private int $currentTransactionLevel = 0;
 
     /**
-     * PDO begin transaction override to work with savepoint capabilities for
-     * supported SGBD. Allows nested transactions.
+     * PDO begin transaction override to work with savepoint capabilities for supported SGBD. Allows for working nested
+     * transactions.
+     *
+     * @return bool
      */
     public function beginTransaction(): bool
     {
         if ($this->currentTransactionLevel == 0 || !$this->nestable()) {
             parent::beginTransaction();
         } else {
-            $this->exec("SAVEPOINT LEVEL{$this->currentTransactionLevel}");
+            $this->exec("SAVEPOINT LEVEL$this->currentTransactionLevel");
         }
         ++$this->currentTransactionLevel;
         return true;
     }
 
     /**
-     * PDO commit override to work with savepoint capabilities for supported
-     * SGBD. Allows nested transactions.
+     * PDO commit override to work with savepoint capabilities for supported SGBD. Allows for working nested
+     * transactions.
+     *
+     * @return bool
      */
     public function commit(): bool
     {
@@ -31,14 +37,16 @@ class DatabaseConnector extends \PDO
         if ($this->currentTransactionLevel == 0 || !$this->nestable()) {
             return parent::commit();
         } else {
-            $this->exec("RELEASE SAVEPOINT LEVEL{$this->currentTransactionLevel}");
+            $this->exec("RELEASE SAVEPOINT LEVEL$this->currentTransactionLevel");
         }
         return true;
     }
 
     /**
-     * PDO rollback override to work with savepoint capabilities for
-     * supported SGBD. Allows nested transactions.
+     * PDO rollback override to work with savepoint capabilities for supported SGBD. Allows for working nested
+     * transactions.
+     *
+     * @return bool
      */
     public function rollBack(): bool
     {
@@ -46,16 +54,18 @@ class DatabaseConnector extends \PDO
         if ($this->currentTransactionLevel == 0 || !$this->nestable()) {
             return parent::rollBack();
         } else {
-            $this->exec("ROLLBACK TO SAVEPOINT LEVEL{$this->currentTransactionLevel}");
+            $this->exec("ROLLBACK TO SAVEPOINT LEVEL$this->currentTransactionLevel");
         }
         return true;
     }
 
     /**
      * Verifies if the current PDO driver supports save points.
+     *
+     * @return bool
      */
     private function nestable(): bool
     {
-        return in_array($this->getAttribute(\PDO::ATTR_DRIVER_NAME), self::$savepointEnabled);
+        return in_array($this->getAttribute(PDO::ATTR_DRIVER_NAME), self::$savepointEnabled);
     }
 }
