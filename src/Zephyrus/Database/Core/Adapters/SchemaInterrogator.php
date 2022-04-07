dcc1fc4240c2480fd9f2133861e8b1517462032a
dadajuice <davidt2003@msn.com>
2022-04-07 08:35:23 -0400

    Added abstract class for schema interrogations

--- src/Zephyrus/Database/Core/Adapters/SchemaInterrogator.php
+++ src/Zephyrus/Database/Core/Adapters/SchemaInterrogator.php
@@ -3,7 +3,12 @@
 use stdClass;
 use Zephyrus\Database\Core\Database;
 
-interface SchemaInterrogation
+/**
+ * Template abstract class used for all supported database management systems to interact with the meta database. This
+ * class should have a specific children for each DBMS adapter with redefined methods matching the proper SQL queries
+ * for each meta database's supported interrogations.
+ */
+abstract class SchemaInterrogator
 {
     /**
      * Meta query to retrieve all table names of given database instance. Must be redefined in children adapter classes
@@ -13,16 +18,7 @@ interface SchemaInterrogation
      * @param Database $database
      * @return string[]
      */
-    public function getAllTableNames(Database $database): array;
-
-    /**
-     * Meta query to retrieve all table details of given database instance. Must be redefined in children adapter
-     * classes to adapt for each supported DBMS. Should return an array of stdClass.
-     *
-     * @param Database $database
-     * @return stdClass[]
-     */
-    public function getAllTables(Database $database): array;
+    public abstract function getAllTableNames(Database $database): array;
 
     /**
      * Meta query to retrieve all column names of given table name within the specified database instance. Must be
@@ -33,7 +29,7 @@ interface SchemaInterrogation
      * @param string $tableName
      * @return string[]
      */
-    public function getAllColumnNames(Database $database, string $tableName): array;
+    public abstract function getAllColumnNames(Database $database, string $tableName): array;
 
     /**
      * Meta query to retrieve all contraints of a specific table. Must be redefined in children adapter classes to
@@ -43,7 +39,7 @@ interface SchemaInterrogation
      * @param string $tableName
      * @return stdClass[]
      */
-    public function getAllConstraints(Database $database, string $tableName): array;
+    public abstract function getAllConstraints(Database $database, string $tableName): array;
 
     /**
      * Meta query to retrieve all column details of given table name within the specified database instance. Must be
@@ -53,5 +49,25 @@ interface SchemaInterrogation
      * @param string $tableName
      * @return stdClass[]
      */
-    public function getAllColumns(Database $database, string $tableName): array;
+    public abstract function getAllColumns(Database $database, string $tableName): array;
+
+    /**
+     * Meta query to retrieve all table details of given database instance. Return an array of stdClasses with the
+     * following properties: name, columns and contraints.
+     *
+     * @param Database $database
+     * @return stdClass[]
+     */
+    public function getAllTables(Database $database): array
+    {
+        $results = [];
+        foreach ($this->getAllTableNames($database) as $name) {
+            $results[] = (object) [
+                'name' => $name,
+                'columns' => $this->getAllColumns($database, $name),
+                'constraints' => $this->getAllConstraints($database, $name)
+            ];
+        }
+        return $results;
+    }
 }
