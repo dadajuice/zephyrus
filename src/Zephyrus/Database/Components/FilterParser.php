88680fa227c29f329f95a92f9a44f0318f3701a2
dadajuice <davidt2003@msn.com>
2022-07-27 13:29:31 -0400

    feat: add allowed filter columns security

--- src/Zephyrus/Database/Components/FilterParser.php
+++ src/Zephyrus/Database/Components/FilterParser.php
@@ -9,6 +9,12 @@ class FilterParser
     public const URL_PARAMETER = 'filters';
 
     private WhereClause $whereClause;
+    private array $allowedColumns;
+
+    public function __construct(array $allowedColumns = [])
+    {
+        $this->allowedColumns = $allowedColumns;
+    }
 
     /**
      * Parses the request parameters to build a corresponding WHERE clause. The parameters should be given following the
@@ -17,7 +23,8 @@ class FilterParser
      *     example.com?filters[column:type]=content
      *
      * The columnConversion array allows specifying correspondance between request parameters and database column (if
-     * developers don't want to expose database column directly in UI links).
+     * developers don't want to expose database column directly in UI links). If a specified column is not allowed it
+     * will be ignored. If no column type is given, the "contains" default will be considered.
      *
      * @param array $columnConversion
      * @return WhereClause
@@ -28,7 +35,13 @@ class FilterParser
         $request = RequestFactory::read();
         $filterColumns = $request->getParameter(self::URL_PARAMETER, []);
         foreach ($filterColumns as $columnDefinition => $content) {
+            if (!str_contains($columnDefinition, ":")) {
+                $columnDefinition = $columnDefinition . ':' . 'contains';
+            }
             list($column, $filterType) = explode(':', $columnDefinition);
+            if (!in_array($column, $this->allowedColumns)) {
+                continue;
+            }
             // TODO: Select between or / and from received data ...
             // TODO: Validate "content" for each type of clause (ex. date_range, number, etc.)
             // TODO: Make private method for all the matches
