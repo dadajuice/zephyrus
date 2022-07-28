b9cb5f4217628b558666d6ff675dcb1dcd7497ad
dadajuice <davidt2003@msn.com>
2022-07-28 08:12:30 -0400

    test: add tests for PagerParser

---
+++ tests/Database/Components/PagerParserTest.php
@@ -0,0 +1,29 @@
+<?php namespace Zephyrus\Tests\Database\Components;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Database\Components\PagerParser;
+use Zephyrus\Network\Request;
+use Zephyrus\Network\RequestFactory;
+
+class PagerParserTest extends TestCase
+{
+    public function testDefaultPager()
+    {
+        $request = new Request("http://example.com/projects", "get", ['parameters' => []]);
+        RequestFactory::set($request);
+        $parser = new PagerParser();
+        $clause = $parser->parse();
+        self::assertEquals("LIMIT 50 OFFSET 0", $clause->getSql());
+    }
+
+    public function testPage()
+    {
+        $request = new Request("http://example.com/projects?page=5", "get", ['parameters' => [
+            'page' => 5
+        ]]);
+        RequestFactory::set($request);
+        $parser = new PagerParser();
+        $clause = $parser->parse();
+        self::assertEquals("LIMIT 50 OFFSET 200", $clause->getSql());
+    }
+}
