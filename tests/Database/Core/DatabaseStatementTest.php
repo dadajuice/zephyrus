f0144f45b170518914ccbe1031b79294ce3b3047
dadajuice <davidt2003@msn.com>
2022-07-29 07:39:50 -0400

    test: add tests for DatabaseStatement

---
+++ tests/Database/Core/DatabaseStatementTest.php
@@ -0,0 +1,23 @@
+<?php namespace Zephyrus\Tests\Database\Core;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Database\Core\Database;
+use Zephyrus\Database\Core\DatabaseSource;
+
+class DatabaseStatementTest extends TestCase
+{
+    public function testHtmlSanitize()
+    {
+        $db = new Database(new DatabaseSource());
+        $db->query('CREATE TABLE heroes(id NUMERIC PRIMARY KEY, name TEXT NULL, enabled INTEGER, power REAL);');
+        $db->query("INSERT INTO heroes(id, name, enabled, power) VALUES (1, 'Batman', 1, 5.6);");
+
+        $db->query("INSERT INTO heroes(id, name) VALUES (2, '<p>superman</p>');");
+        $result = $db->query("SELECT * FROM heroes WHERE id = 2");
+        $result->setSanitizeCallback(function ($value) {
+            return strip_tags($value);
+        });
+        $row = $result->next();
+        self::assertEquals('superman', $row->name);
+    }
+}
