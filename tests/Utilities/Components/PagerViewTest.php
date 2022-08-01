45857640099240725112d42b6fbcb0b898e0978a
dadajuice <davidt2003@msn.com>
2022-08-01 10:44:22 -0400

    test: add test for PagerView

--- tests/Utilities/Components/PagerViewTest.php
+++ tests/Utilities/Components/PagerViewTest.php
@@ -63,6 +63,15 @@ class PagerViewTest extends TestCase
         self::assertEquals('<div class="pager"><a href="#?page=1">&lt;</a><a href="#?page=1">1</a><span>2</span><a href="#?page=3">3</a><a href="#?page=4">4</a><a href="#?page=5">5</a><a href="#?page=6">6</a><a href="#?page=7">7</a><a href="#?page=8">8</a><a href="#?page=9">9</a><a href="#?page=3">&gt;</a><a href="#?page=16">»</a></div>', $html);
     }
 
+    public function testDisplayFirstOfSixteenPagesValidated()
+    {
+        $model = new PagerModel(50, 50);
+        $model->setCurrentPage(900);
+        $view = new PagerView($model, 760); // 16 pages ...
+        $html = $view->getHtml(); // Will go to page one, since page 900 doesn't exist.
+        self::assertEquals('<div class="pager"><span>1</span><a href="#?page=2">2</a><a href="#?page=3">3</a><a href="#?page=4">4</a><a href="#?page=5">5</a><a href="#?page=6">6</a><a href="#?page=7">7</a><a href="#?page=8">8</a><a href="#?page=9">9</a><a href="#?page=2">&gt;</a><a href="#?page=16">»</a></div>', $html);
+    }
+
     public function testDisplayTenthOfSixteenPages()
     {
         $model = new PagerModel(50, 50);
