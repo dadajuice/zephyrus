269c580838aa93ea995b7fab29e8bacca758e673
dadajuice <davidt2003@msn.com>
2022-08-01 10:17:32 -0400

    refactor: Pager parser now returns a PagerModel instance

--- src/Zephyrus/Utilities/Components/PagerParser.php
+++ src/Zephyrus/Utilities/Components/PagerParser.php
@@ -1,7 +1,7 @@
-<?php namespace Zephyrus\Database\Components;
+<?php namespace Zephyrus\Utilities\Components;
 
-use Zephyrus\Database\QueryBuilder\LimitClause;
 use Zephyrus\Network\RequestFactory;
+use Zephyrus\Network\Uri;
 
 class PagerParser
 {
@@ -35,19 +35,17 @@ class PagerParser
     }
 
     /**
-     * Parses the request parameters to build a corresponding LIMIT clause. The parameters should be given following the
-     * public constants:
+     * Parses the request parameters to build a corresponding PagerModel instance. The parameters should be given
+     * following the public constants:
      *
      *     example.com?page=4&limit=96
      *
      * The limit parameter is optional, as the default value (50 per page) will be used if none given. It cannot go
      * beyond the configured max limit allowed for security reason (avoid a user to manually select 15000 rows per
      * page). Developers should indicate the maximum allowed when permitting user to change the row count. By default,
-     * it is limited to 50.
-     *
-     * @return LimitClause
+     * it is limited to 50 (same as the default rows per page).
      */
-    public function parse(): LimitClause
+    public function parse(): PagerModel
     {
         $request = RequestFactory::read();
         $page = $request->getParameter(self::URL_PARAMETER, 1);
@@ -55,6 +53,11 @@ class PagerParser
         $limit = min($limit, $this->maxLimitAllowed);
         $currentPage = (!is_numeric($page) || $page < 0) ? 1 : $page;
         $offset = $limit * ($currentPage - 1);
-        return new LimitClause($limit, $offset);
+
+        $model = new PagerModel($limit, $offset);
+        $model->setCurrentPage($currentPage);
+        $model->setPageUrl($request->getUri()->getPath());
+        $model->setPageQuery(Uri::removeArgument($request->getUri()->getQuery(), self::URL_PARAMETER));
+        return $model;
     }
 }
