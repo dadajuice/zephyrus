b5e6db29743ae9ee2682dbfefa346830fe3b937a
dadajuice <davidt2003@msn.com>
2022-08-03 07:27:55 -0400

    test: add database wrapper test case abstract class

---
+++ tests/Database/DatabaseTestCase.php
@@ -0,0 +1,32 @@
+<?php namespace Zephyrus\Tests\Database;
+
+use PHPUnit\Framework\TestCase;
+use Zephyrus\Application\Configuration;
+use Zephyrus\Database\Core\Database;
+
+abstract class DatabaseTestCase extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        $db = new Database(Configuration::getDatabaseConfiguration());
+        $db->query("DROP TABLE IF EXISTS heroes");
+        $db->query('CREATE TABLE heroes(id SERIAL PRIMARY KEY, name TEXT NULL, alter TEXT NULL, power FLOAT)');
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (1, 'Batman', 'Bruce Wayne', 20.56);");
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (2, 'Superman', 'Clark Kent', 10.10);");
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (3, 'Aquaman', 'Arthur Curry', 23.50);");
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (4, 'Wonder Woman', 'Diana Prince', 12.67);");
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (5, 'Flash', 'Barry Allan', 5.89);");
+        $db->query("INSERT INTO heroes(id, name, alter, power) VALUES (6, 'Green Lantern', 'Hal Jordan', 12.12);");
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        $db = new Database(Configuration::getDatabaseConfiguration());
+        $db->query("DROP TABLE heroes");
+    }
+
+    public function buildDatabase(): Database
+    {
+        return new Database(Configuration::getDatabaseConfiguration());
+    }
+}
