ba601ba5013881c7cbb66344ff50bedf9c76a2e4
David Tucker <davidt2003@msn.com>
2023-07-10 08:42:32 -0400

    feat: add Pagination class

---
+++ src/Zephyrus/Utilities/Listing/ListPagination.php
@@ -0,0 +1,56 @@
+<?php namespace Zephyrus\Utilities\Listing;
+
+use InvalidArgumentException;
+
+class ListPagination
+{
+    public const DEFAULT_LIMIT = 50;
+
+    private ?int $currentPage;
+    private ?int $limit;
+    private int $defaultLimit = self::DEFAULT_LIMIT;
+    private int $maxLimitAllowed = self::DEFAULT_LIMIT;
+
+    public function __construct(mixed $currentPage, mixed $limit)
+    {
+        $this->currentPage = $this->parseValue($currentPage);
+        $this->limit = $this->parseValue($limit);
+    }
+
+    public function getCurrentPage(): int
+    {
+        return (!is_numeric($this->currentPage) || $this->currentPage < 0) ? 1 : $this->currentPage;
+    }
+
+    public function getLimit(): int
+    {
+        return min($this->limit ?? $this->defaultLimit, $this->maxLimitAllowed);
+    }
+
+    public function getMaxLimit(): int
+    {
+        return $this->maxLimitAllowed;
+    }
+
+    public function getMaxPage(int $recordCount): int
+    {
+        return ceil($recordCount / $this->getLimit());
+    }
+
+    public function setDefaultLimit(int $defaultLimit, int $maxLimitAllowed): void
+    {
+        if ($maxLimitAllowed < $defaultLimit) {
+            throw new InvalidArgumentException("The limit must be greater or equal to the default limit.");
+        }
+        $this->defaultLimit = $defaultLimit;
+        $this->maxLimitAllowed = $maxLimitAllowed;
+    }
+
+    private function parseValue(mixed $rawPage): ?int
+    {
+        if (!is_numeric($rawPage ?? "")) {
+            return null;
+        }
+        return (int) $rawPage;
+    }
+}
