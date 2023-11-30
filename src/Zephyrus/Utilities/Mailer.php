ec31adc054d5171cf5ef2e054ffc01906ad7af4c
David Tucker <davidt2003@msn.com>
2023-11-30 18:10:05 -0500

    feat: simplify mailer

--- src/Zephyrus/Utilities/Mailer.php
+++ src/Zephyrus/Utilities/Mailer.php
@@ -1,28 +1,21 @@
 <?php namespace Zephyrus\Utilities;
 
+use InvalidArgumentException;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use Zephyrus\Utilities\FileSystem\File;
 
 class Mailer
 {
     private PHPMailer $phpMailer;
-    private MailerSmtpConfiguration $configuration;
-    private bool $useSmtp;
+    private MailerSmtpConfiguration $smtpConfiguration;
 
     public function __construct(MailerSmtpConfiguration $configuration)
     {
-        $this->configuration = $configuration;
+        $this->smtpConfiguration = $configuration;
         $this->phpMailer = new PHPMailer();
         $this->phpMailer->CharSet = 'UTF-8';
         $this->initializeSmtp();
-    }
-
-    public function build(string $subject): Email
-    {
-        $instance = clone $this->phpMailer;
-        $instance->clearAllRecipients();
-        $instance->clearAttachments();
-        return new Email($instance, $this->useSmtp, $subject);
     }
 
     public function getPhpMailer(): PHPMailer
@@ -45,17 +38,189 @@ class Mailer
         }
     }
 
+    public function send(bool $asHtml = true): string
+    {
+        $this->phpMailer->IsHTML($asHtml);
+        if (!$this->phpMailer->preSend()) {
+            echo $this->phpMailer->ErrorInfo;
+            //TODO: throw exception
+        }
+        if ($this->smtpConfiguration->isEnabled()) {
+            if (!$this->phpMailer->postSend()) {
+                echo $this->phpMailer->ErrorInfo;
+                //TODO: throw exception
+            }
+        }
+        return $this->phpMailer->getSentMIMEMessage();
+    }
+
+    public function clearAttachments(): void
+    {
+        $this->phpMailer->clearAttachments();
+    }
+
+    public function clearRecipients(): void
+    {
+        $this->phpMailer->clearAllRecipients();
+    }
+
+    public function setSubject(string $subject): void
+    {
+        $this->phpMailer->Subject = $subject;
+    }
+
+    /**
+     * Applies the given $content as the email body that will be sent to recipients. The $altContent is used for clients
+     * which cannot render HTML content.
+     *
+     * @param string $content
+     * @param string $altContent
+     */
+    public function setBody(string $content, string $altContent = ""): void
+    {
+        $this->phpMailer->Body = $content;
+        $this->phpMailer->AltBody = $altContent;
+    }
+
+    public function addRecipient(string $email, string $name = ""): void
+    {
+        try {
+            $this->phpMailer->addAddress($email, $name);
+        } catch (Exception $e) {
+            // TODO: Invalid Address ...
+        }
+    }
+
+    public function addRecipientCc(string $email, string $name = ""): void
+    {
+        try {
+            $this->phpMailer->addCC($email, $name);
+        } catch (Exception $e) {
+            // TODO: Invalid Address ...
+        }
+    }
+
+    public function addRecipientBcc(string $email, string $name = ""): void
+    {
+        try {
+            $this->phpMailer->addBCC($email, $name);
+        } catch (Exception $e) {
+            // TODO: Invalid Address ...
+        }
+    }
+
+    public function addRecipients(array $recipients): void
+    {
+        foreach ($recipients as $email => $name) {
+            $this->addRecipient($email, $name);
+        }
+    }
+
+    public function addReplyTo(string $email, string $name = ""): void
+    {
+        try {
+            $this->phpMailer->addReplyTo($email, $name);
+        } catch (Exception $e) {
+            // TODO: Invalid Address ...
+        }
+    }
+
+    /**
+     * Adds an attachment from the filesystem. The file will then be accessible for download by the recipients. The
+     * given path must point to an existing and accessible file on the filesystem, throws an exception otherwise. If
+     * the $filename argument is supplied, this will override the original filename in the mail (so when users download
+     * the file, they will have this filename). If you need to add an attachment from a URL, make sure to fetch it
+     * beforehand and store it within your filesystem and then use this method.
+     *
+     * @param string $path
+     * @param string|null $filename
+     */
+    public function addAttachment(string $path, ?string $filename = null): void
+    {
+        try {
+            $attachment = new File($path);
+            $this->phpMailer->addAttachment($path, $filename ?? $attachment->getFilename(),
+                PHPMailer::ENCODING_BASE64, $attachment->getMimeType());
+        } catch (InvalidArgumentException $exception) {
+            // TODO: File not exists ...
+        } catch (Exception $exception) {
+            // TODO: Mailer Exception ...
+        }
+    }
+
+    /**
+     * Adds an embedded (inline) attachment from a file. This can include images, sounds, and just about any other
+     * document type. These differ from 'regular' attachments in which they are intended to be displayed inline with
+     * the message, not just attached for download. This is used in HTML messages that embed the images the HTML refers
+     * to using the `$cid` value in `img` tags, for example `<img src="cid:mylogo">`.
+     *
+     * @param string $path
+     * @param string $cid
+     * @param string|null $filename
+     */
+    public function addInlineAttachment(string $path, string $cid, ?string $filename = null): void
+    {
+        try {
+            $attachment = new File($path);
+            $this->phpMailer->addEmbeddedImage($path, $cid, $filename ?? $attachment->getFilename(),
+                PHPMailer::ENCODING_BASE64, $attachment->getMimeType());
+        } catch (InvalidArgumentException $exception) {
+            // TODO: File not exists ...
+        } catch (Exception $exception) {
+            // TODO: Mailer Exception ...
+        }
+    }
+
+    /**
+     * Adds an attachment from a string or binary attachment (non-filesystem) that will be accessible for download by
+     * the recipients. This method can be used to attach ascii or binary data, such as a BLOB record from a database.
+     * Filename must be supplied including the file extension. Optionally, the $mimeType can be given for precise
+     * content type information for the attachment. Leave null to let the PHPMailer algorithm figure out the proper
+     * mime type from the given filename extension.
+     *
+     * @param string $string
+     * @param string $filename
+     * @param string|null $mimeType
+     */
+    public function addBinaryAttachment(string $string, string $filename, ?string $mimeType = null): void
+    {
+        try {
+            $this->phpMailer->addStringAttachment($string, $filename,
+                PHPMailer::ENCODING_BASE64, $mimeType ?? '');
+        } catch (Exception $exception) {
+            // TODO: Mailer Exception ...
+        }
+    }
+
+    /**
+     * Adds an embedded (inline) attachment from a string or binary (non-filesystem) that will be accessible within the
+     * email structure (see method addBinaryAttachment).
+     *
+     * @param string $string
+     * @param string $filename
+     * @param string|null $mimeType
+     * @return void
+     */
+    public function addBinaryInlineAttachment(string $string, string $filename, ?string $mimeType = null): void
+    {
+        try {
+            $this->phpMailer->addStringAttachment($string, $filename,
+                PHPMailer::ENCODING_BASE64, $mimeType ?? '', 'inline');
+        } catch (Exception $exception) {
+            // TODO: Mailer Exception ...
+        }
+    }
+
     private function initializeSmtp(): void
     {
-        $this->useSmtp = $this->configuration->isEnabled();
-        if ($this->useSmtp) {
+        if ($this->smtpConfiguration->isEnabled()) {
             $this->phpMailer->isSMTP();
-            $this->phpMailer->Host = $this->configuration->getHost();
-            $this->phpMailer->SMTPAuth = $this->configuration->hasAuthentication();
-            $this->phpMailer->Username = $this->configuration->getUsername();
-            $this->phpMailer->Password = $this->configuration->getPassword();
-            $this->phpMailer->SMTPSecure = $this->configuration->getEncryption();
-            $this->phpMailer->Port = $this->configuration->getPort();
+            $this->phpMailer->Host = $this->smtpConfiguration->getHost();
+            $this->phpMailer->SMTPAuth = $this->smtpConfiguration->hasAuthentication();
+            $this->phpMailer->Username = $this->smtpConfiguration->getUsername();
+            $this->phpMailer->Password = $this->smtpConfiguration->getPassword();
+            $this->phpMailer->SMTPSecure = $this->smtpConfiguration->getEncryption();
+            $this->phpMailer->Port = $this->smtpConfiguration->getPort();
         }
     }
 }
